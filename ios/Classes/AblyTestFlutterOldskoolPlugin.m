6173dd276534d0c977bdd92a27caa311451418e9
Quintin Willison <quintin@ably.io>
2020-01-21 16:19:54 +0000

    Fix Objective-C indentation.

--- ios/Classes/AblyTestFlutterOldskoolPlugin.m
+++ ios/Classes/AblyTestFlutterOldskoolPlugin.m
@@ -1,20 +1,21 @@
 #import "AblyTestFlutterOldskoolPlugin.h"
 
 @implementation AblyTestFlutterOldskoolPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"ably_test_flutter_oldskool_plugin"
-            binaryMessenger:[registrar messenger]];
-  AblyTestFlutterOldskoolPlugin* instance = [[AblyTestFlutterOldskoolPlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+
++(void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+    FlutterMethodChannel* channel =
+        [FlutterMethodChannel methodChannelWithName:@"ably_test_flutter_oldskool_plugin"
+                                    binaryMessenger:[registrar messenger]];
+    AblyTestFlutterOldskoolPlugin* instance = [[AblyTestFlutterOldskoolPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
 
-- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
+-(void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+    if ([@"getPlatformVersion" isEqualToString:call.method]) {
+        result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+    } else {
+        result(FlutterMethodNotImplemented);
+    }
 }
 
 @end
