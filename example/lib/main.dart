0af2b9d96fb0de5c56005ed7c67738f5ce9d5cdd
Quintin Willison <quintin@ably.io>
2020-01-23 13:03:55 +0000

    Add pure Dart solution to provision an Ably key from the sandbox environment.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 import 'package:ably_test_flutter_oldskool_plugin/ably_test_flutter_oldskool_plugin.dart';
+import 'package:ably_test_flutter_oldskool_plugin_example/provisioning.dart' as provisioning;
 
 void main() => runApp(MyApp());
 
@@ -11,9 +12,18 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
+enum OpState {
+  NotStarted,
+  InProgress,
+  Succeeded,
+  Failed,
+}
+
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
   String _ablyVersion = 'Unknown';
+  OpState _provisioningState = OpState.NotStarted;
+  provisioning.AppKey _appKey;
 
   @override
   void initState() {
@@ -52,6 +62,50 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+  void provisionAbly() async {
+    setState(() { _provisioningState = OpState.InProgress; });
+    
+    provisioning.AppKey appKey;
+    try {
+      appKey = await provisioning.provision('sandbox-');
+    } catch (error) {
+      setState(() { _provisioningState = OpState.Failed; });
+      return;
+    }
+    
+    setState(() {
+      _appKey = appKey;
+      _provisioningState = OpState.Succeeded;
+    });
+  }
+
+  String opStateDescription(OpState state, String action, String operating, String done) {
+    switch (state) {
+      case OpState.NotStarted: return action;
+      case OpState.InProgress: return operating + '...';
+      case OpState.Succeeded: return done;
+      case OpState.Failed: return 'Failed to ' + action;
+    }
+  }
+
+  Color opStateColor(OpState state) {
+    switch (state) {
+      case OpState.NotStarted: return Color.fromARGB(255, 192, 192, 255);
+      case OpState.InProgress: return Color.fromARGB(255, 192, 192, 192);
+      case OpState.Succeeded: return Color.fromARGB(255, 128, 255, 128);
+      case OpState.Failed: return Color.fromARGB(255, 255, 128, 128);
+    }
+  }
+
+  Widget provisionButton() => FlatButton(
+    onPressed: (_provisioningState == OpState.NotStarted || _provisioningState == OpState.Failed) ? provisionAbly : null,
+    child: Text(
+      opStateDescription(_provisioningState, 'Provision Ably', 'Provisioning Ably', 'Ably Provisioned'),
+    ),
+    color: opStateColor(_provisioningState),
+    disabledColor: opStateColor(_provisioningState),
+  );
+
   @override
   Widget build(BuildContext context) {
     print('widget build');
@@ -65,6 +119,8 @@ class _MyAppState extends State<MyApp> {
             children: [
               Text('Running on: $_platformVersion\n'),
               Text('Ably version: $_ablyVersion\n'),
+              provisionButton(),
+              Text('App Key: ' + ((_appKey == null) ? 'Ably not provisioned yet.' : _appKey.toString())),
             ]
           ),
         ),
---
+++ example/lib/provisioning.dart
@@ -0,0 +1,53 @@
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+const _capabilitySpec = const {
+  // Allow us to publish and subscribe to any channel.
+  '*': ['publish', 'subscribe'],
+};
+
+// per: https://docs.ably.io/client-lib-development-guide/test-api/
+final _appSpec = Map.unmodifiable({
+  // API Keys & Capabilities.
+  'keys': [
+    {
+      // The need to use jsonEncode here is a requirement of the Sandbox Test API.
+      // The capability map has to be JSON encoded as a string and then appropriately
+      // escaped in order for presentation within a string value.
+      'capability': jsonEncode(_capabilitySpec),
+    },
+  ],
+});
+
+const _requestHeaders = const {
+  'Content-Type': 'application/json',
+  'Accept': 'application/json',
+};
+
+class AppKey {
+  final String name;
+  final String secret;
+  final String _keyStr;
+
+  AppKey(this.name, this.secret, this._keyStr);
+  
+  @override
+  String toString() => _keyStr;
+}
+
+Future<AppKey> provision(final String environmentPrefix) async {
+  final url = 'https://' + environmentPrefix + 'rest.ably.io/apps';
+  final body = jsonEncode(_appSpec);
+  final response = await http.post(url, body: body, headers: _requestHeaders);
+  if (response.statusCode != HttpStatus.created) {
+    throw HttpException('Server didn\'t return success. Status: ' + response.statusCode.toString());
+  }
+
+  // Cherry pick the pieces from the JSON response that we need.
+  final Map result = jsonDecode(response.body);
+  final List keys = result['keys'];
+  final Map key = keys[0];
+  return AppKey(key['keyName'], key['keySecret'], key['keyStr']);
+}
