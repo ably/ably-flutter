b25df0fe89e8d885e115a54d04164c508b35354b
Quintin Willison <quintin@ably.io>
2020-01-27 11:29:20 +0000

    Add partially complete custom message codec on the Dart side which now causes the iOS platform code to crash `flutter run` at the point the 'Create Ably Realtime' button is pressed, with:
    *** First throw call stack:
    (
    	0   CoreFoundation                      0x00007fff23c4f02e __exceptionPreprocess + 350
    	1   libobjc.A.dylib                     0x00007fff50b97b20 objc_exception_throw + 48
    	2   CoreFoundation                      0x00007fff23c4eda8 +[NSException raise:format:arguments:] + 88
    	3   Foundation                          0x00007fff256c9b61 -[NSAssertionHandler handleFailureInMethod:object:file:lineNumber:description:] + 191
    	4   Flutter                             0x000000010120745b -[FlutterStandardReader readValueOfType:] + 205
    	5   Flutter                             0x0000000101205bc6 -[FlutterStandardMethodCodec decodeMethodCall:] + 83
    	6   Flutter                             0x0000000101203f52 __45-[FlutterMethodChannel setMethodCallHandler:]_block_invoke + 37
    	7   Flutter                             0x0000000101197d80 _ZNK7flutter21PlatformMessageRout<…>
    Lost connection to device.

--- lib/src/ably.dart
+++ lib/src/ably.dart
@@ -1,8 +1,12 @@
 import 'dart:async';
+import 'dart:ffi';
 import 'package:flutter/services.dart';
+import 'package:flutter/foundation.dart';
 import 'package:ably_test_flutter_oldskool_plugin/ably.dart' as api;
 
-const MethodChannel methodChannel = MethodChannel('ably_test_flutter_oldskool_plugin');
+/// Uses our custom message codec so that we can pass Ably types to the
+/// platform implementations.
+final methodChannel = MethodChannel('ably_test_flutter_oldskool_plugin', StandardMethodCodec(Codec()));
 
 Future<String> get platformVersion async {
   return await methodChannel.invokeMethod('getPlatformVersion');
@@ -44,4 +48,66 @@ class Realtime extends PlatformObject implements api.Realtime {
   @override
   // TODO: implement connection
   api.Connection get connection => null;
+}
+
+class Codec extends StandardMessageCodec {
+  // Custom type values must be over 127. At the time of writing the standard message
+  // codec encodes them as an unsigned byte which means the maximum type value is 255.
+  // If we get to the point of having more than that number of custom types (i.e. more
+  // than 128 [255 - 127]) then we can either roll our own codec from scatch or,
+  // perhaps easier, reserve custom type value 255 to indicate that it will be followed
+  // by a subtype value - perhaps of a wider type.
+  // https://api.flutter.dev/flutter/services/StandardMessageCodec/writeValue.html
+  static const _valueClientOptions = 128;
+
+  @override
+  void writeValue (final WriteBuffer buffer, final dynamic value) {
+    if (value is api.ClientOptions) {
+      buffer.putUint8(_valueClientOptions);
+      final api.ClientOptions v = value;
+
+      // AuthOptions (super class of ClientOptions)
+      writeValue(buffer, v.authCallback);
+      writeValue(buffer, v.authUrl);
+      writeValue(buffer, v.authMethod);
+      writeValue(buffer, v.key);
+      writeValue(buffer, v.tokenDetails);
+      writeValue(buffer, v.authHeaders);
+      writeValue(buffer, v.authParams);
+      writeValue(buffer, v.queryTime);
+      writeValue(buffer, v.useAuthToken);
+
+      // ClientOptions
+      writeValue(buffer, v.clientId);
+      writeValue(buffer, v.logLevel);
+      writeValue(buffer, v.logHandler);
+      writeValue(buffer, v.tls);
+      writeValue(buffer, v.restHost);
+      writeValue(buffer, v.realtimeHost);
+      writeValue(buffer, v.port);
+      writeValue(buffer, v.tlsPort);
+      writeValue(buffer, v.autoConnect);
+      writeValue(buffer, v.useBinaryProtocol);
+      writeValue(buffer, v.queueMessages);
+      writeValue(buffer, v.echoMessages);
+      writeValue(buffer, v.recover);
+      writeValue(buffer, v.proxy);
+      writeValue(buffer, v.environment);
+      writeValue(buffer, v.idempotentRestPublishing);
+      writeValue(buffer, v.httpOpenTimeout);
+      writeValue(buffer, v.httpRequestTimeout);
+      writeValue(buffer, v.httpMaxRetryCount);
+      writeValue(buffer, v.realtimeRequestTimeout);
+      writeValue(buffer, v.fallbackHosts);
+      writeValue(buffer, v.fallbackHostsUseDefault);
+      writeValue(buffer, v.fallbackRetryTimeout);
+      writeValue(buffer, v.defaultTokenParams);
+      writeValue(buffer, v.channelRetryTimeout);
+      writeValue(buffer, v.transportParams);
+      writeValue(buffer, v.asyncHttpThreadpoolSize);
+      writeValue(buffer, v.pushFullWait);
+    } else {
+      super.writeValue(buffer, value);
+    }
+  }
 }
