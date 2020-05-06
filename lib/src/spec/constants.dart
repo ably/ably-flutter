c8cee62df525fbcf032d5b1a7dbcdce93b68ab25
Rohit R. Abbadi <rohit@infito.com>
2020-05-07 00:55:13 +0530

    fixup LogLevel constants usage, and make the constant declarations as const instead of final

--- lib/src/spec/constants.dart
+++ lib/src/spec/constants.dart
@@ -1,8 +1,8 @@
 class LogLevel{
-  static final int none = 99;
-  static final int verbose = 2;
-  static final int debug = 3;
-  static final int info = 4;
-  static final int warn = 5;
-  static final int error = 6;
+  static const int none = 99;
+  static const int verbose = 2;
+  static const int debug = 3;
+  static const int info = 4;
+  static const int warn = 5;
+  static const int error = 6;
 }
