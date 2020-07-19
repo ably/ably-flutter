f43bc8b2fb7d85b561b48cc6435a323cdc752891
Rohit R. Abbadi <rohit@flackon.com>
2020-07-19 13:01:41 +0530

    `ablyEventMessage` related definitions, encoder/decoders

--- ios/Classes/AblyFlutterMessage.h
+++ ios/Classes/AblyFlutterMessage.h
@@ -15,4 +15,18 @@ NS_ASSUME_NONNULL_BEGIN
 
 @end
 
+
+@interface AblyFlutterEventMessage : NSObject
+
++(instancetype)new NS_UNAVAILABLE;
+-(instancetype)init NS_UNAVAILABLE;
+
+-(instancetype)initWithEventName:(NSString *)eventName
+                      message:(id)message NS_DESIGNATED_INITIALIZER;
+
+@property(nonatomic, readonly) NSString * eventName;
+@property(nonatomic, readonly) id message;
+
+@end
+
 NS_ASSUME_NONNULL_END
