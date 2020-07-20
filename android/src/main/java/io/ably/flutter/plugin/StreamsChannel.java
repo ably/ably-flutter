86df27e38e1dafead6485b55270e8237e8db8b14
Quintin Willison <quintin@ably.io>
2020-07-20 17:41:45 +0100

    Update copyright and license headers for source code derived from Loup sources.

--- android/src/main/java/io/ably/flutter/plugin/StreamsChannel.java
+++ android/src/main/java/io/ably/flutter/plugin/StreamsChannel.java
@@ -1,8 +1,22 @@
-// This file is derivative of work derived from  original work at https://github.com/loup-v/streams_channel
-// Copyright (c) 2018 Loup Inc.
-// Licensed under Apache License v2.0
-// Original file @ https://github.com/loup-v/streams_channel/blob/master/android/src/main/java/app/loup/streams_channel/StreamsChannel.java
-
+/*
+ * This file is derivative of work derived from original work at:
+ * https://github.com/loup-v/streams_channel
+ * 
+ * Copyright 2018 Loup Inc.
+ * Copyright 2020 Ably Real-time Ltd (ably.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package io.ably.flutter.plugin;
 
