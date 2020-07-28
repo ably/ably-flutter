2d8004cb8dcdac1b4a9f2061e6cc8b3b4444384e
Rohit R. Abbadi <rohit@flackon.com>
2020-07-28 20:24:17 +0530

    enhance example for publishing messages

--- example/test_harness/publish.js
+++ example/test_harness/publish.js
@@ -3,7 +3,7 @@ const Ably = require('ably');
 if(process.argv[2] && !process.argv[3]) throw new Error("Invalid number of args: pass either 0 or 2 args");
 
 const key = process.argv[2];
-const channelName = process.argv[3];
+const channelName = process.argv[3] || 'test-channel';
 const environment = 'sandbox';
 const logLevel = 2;
 
