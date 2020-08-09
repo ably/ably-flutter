467499fcab6d4e814f7b21e57770bcd8fda5dae2
Rohit R. Abbadi <rohit@flackon.com>
2020-08-10 00:07:56 +0530

    extending ARTClientOptions to support a new boolean hasAuthCallback to manage callbacks

---
+++ ios/Classes/types/ARTClientOptions+AblyFlutterClientOptions.h
@@ -0,0 +1,11 @@
+#import "ARTClientOptions.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ARTClientOptions (AblyFlutterClientOptions)
+
+@property (readwrite) BOOL hasAuthCallback;
+
+@end
+
+NS_ASSUME_NONNULL_END
