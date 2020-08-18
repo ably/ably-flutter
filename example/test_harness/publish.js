ae83eba0d0814230a32aeb51fe1bee37112b12db
Rohit R. Abbadi <rohit@flackon.com>
2020-08-18 10:09:53 +0530

    improve test-harness code
    1. var -> const
    2. code formatting using `prettier` playground: https://prettier.io/playground/

--- example/test_harness/publish.js
+++ example/test_harness/publish.js
@@ -1,69 +1,72 @@
-const Ably = require('ably');
+const Ably = require("ably");
 
-if(process.argv[2] && !process.argv[3]) throw new Error("Invalid number of args: pass either 0 or 2 args");
+if (process.argv[2] && !process.argv[3]) {
+  throw new Error("Invalid number of args: pass either 0 or 2 args");
+}
 
 const key = process.argv[2];
-const channelName = process.argv[3] || 'test-channel';
-const environment = 'sandbox';
+const channelName = process.argv[3] || "test-channel";
+const environment = "sandbox";
 const logLevel = 2;
 
-var realtime = new Ably.Realtime({key, environment, logLevel});
-var channel = realtime.channels.get(channelName);
+const realtime = new Ably.Realtime({ key, environment, logLevel });
+const channel = realtime.channels.get(channelName);
 
-async function publish(name, data){
-    return new Promise((resolve, reject)=>{
-        channel.publish(name, data,
-            function(err) {
-                if(err) {
-                    console.log('publish failed with error ' + err);
-                    reject();
-                } else {
-                    console.log(`published message "${data}" with name "${name}"`);
-                    resolve();
-                }
-            }
-        );
+async function publish(name, data) {
+  return new Promise((resolve, reject) => {
+    channel.publish(name, data, function (err) {
+      if (err) {
+        console.log("publish failed with error " + err);
+        reject();
+      } else {
+        console.log(`published message "${data}" with name "${name}"`);
+        resolve();
+      }
     });
+  });
 }
 
-async function publishUndefined(){
-    await publish('message-undefined', undefined);
+async function publishUndefined() {
+  await publish("message-undefined", undefined);
 }
 
-async function publishNull(){
-    await publish('message-null', null);
+async function publishNull() {
+  await publish("message-null", null);
 }
 
-async function publishString(){
-    await publish('message-string', "Out of boredom! Need some more...");
+async function publishString() {
+  await publish("message-string", "Out of boredom! Need some more...");
 }
 
-async function publishObject(){
-    await publish(
-        'message-data',
-        {"I am": null, "and": {"also": "nested", "too": {"deep": true}}}
-    );
+async function publishObject() {
+  await publish("message-data", {
+    "I am": null,
+    and: { also: "nested", too: { deep: true } },
+  });
 }
 
-async function publishArray(){
-    await publish(
-        'message-data',
-        [42, {"are": "you"}, "crazy", false, {"I am": null, "and": {"also": "nested", "too": {"deep": true}}}]
-    );
+async function publishArray() {
+  await publish("message-data", [
+    42,
+    { are: "you" },
+    "crazy",
+    false,
+    { "I am": null, and: { also: "nested", too: { deep: true } } },
+  ]);
 }
 
-async function delay(fn){
-    return new Promise(async (resolve, reject) => {
-        setTimeout(async () => {
-            resolve(await fn());
-        }, 2000);
-    });
+async function delay(fn) {
+  return new Promise(async (resolve, reject) => {
+    setTimeout(async () => {
+      resolve(await fn());
+    }, 2000);
+  });
 }
 
 publishUndefined()
-    .then(() => delay(publishNull))
-    .then(() => delay(publishString))
-    .then(() => delay(publishObject))
-    .then(() => delay(publishArray))
-    .then(process.exit)
-    .catch((e)=>console.log(e)||process.exit());
+  .then(() => delay(publishNull))
+  .then(() => delay(publishString))
+  .then(() => delay(publishObject))
+  .then(() => delay(publishArray))
+  .then(process.exit)
+  .catch((e) => console.log(e) || process.exit());
