02e6168fd73255ebe5bda6180a24fb0cb3257c37
Rohit R. Abbadi <rohit@flackon.com>
2020-08-18 11:41:27 +0530

    post rebase fix: codegen

--- lib/src/generated/platformconstants.dart
+++ lib/src/generated/platformconstants.dart
@@ -3,36 +3,36 @@
 // source file can be found at bin/templates'
 //
 
-class CodecTypes{
-	static const int ablyMessage = 128;
-	static const int ablyEventMessage = 129;
-	static const int clientOptions = 130;
-	static const int message = 131;
-	static const int tokenParams = 132;
-	static const int tokenDetails = 133;
-	static const int tokenRequest = 134;
-	static const int errorInfo = 144;
-	static const int connectionStateChange = 201;
-	static const int channelStateChange = 202;
+class CodecTypes {
+    static const int ablyMessage = 128;
+    static const int ablyEventMessage = 129;
+    static const int clientOptions = 130;
+    static const int message = 131;
+    static const int tokenParams = 132;
+    static const int tokenDetails = 133;
+    static const int tokenRequest = 134;
+    static const int errorInfo = 144;
+    static const int connectionStateChange = 201;
+    static const int channelStateChange = 202;
 }
 
 class PlatformMethod {
-	static const String getPlatformVersion = 'getPlatformVersion';
-	static const String getVersion = 'getVersion';
-	static const String registerAbly = 'registerAbly';
-	static const String authCallback = 'authCallback';
-	static const String createRestWithOptions = 'createRestWithOptions';
-	static const String publish = 'publish';
-	static const String createRealtimeWithOptions = 'createRealtimeWithOptions';
-	static const String connectRealtime = 'connectRealtime';
-	static const String closeRealtime = 'closeRealtime';
-	static const String attachRealtimeChannel = 'attachRealtimeChannel';
-	static const String detachRealtimeChannel = 'detachRealtimeChannel';
-	static const String setRealtimeChannelOptions = 'setRealtimeChannelOptions';
-	static const String publishRealtimeChannelMessage = 'publishRealtimeChannelMessage';
-	static const String onRealtimeConnectionStateChanged = 'onRealtimeConnectionStateChanged';
-	static const String onRealtimeChannelStateChanged = 'onRealtimeChannelStateChanged';
-	static const String onRealtimeChannelMessage = 'onRealtimeChannelMessage';
+    static const String getPlatformVersion = 'getPlatformVersion';
+    static const String getVersion = 'getVersion';
+    static const String registerAbly = 'registerAbly';
+    static const String authCallback = 'authCallback';
+    static const String createRestWithOptions = 'createRestWithOptions';
+    static const String publish = 'publish';
+    static const String createRealtimeWithOptions = 'createRealtimeWithOptions';
+    static const String connectRealtime = 'connectRealtime';
+    static const String closeRealtime = 'closeRealtime';
+    static const String attachRealtimeChannel = 'attachRealtimeChannel';
+    static const String detachRealtimeChannel = 'detachRealtimeChannel';
+    static const String setRealtimeChannelOptions = 'setRealtimeChannelOptions';
+    static const String publishRealtimeChannelMessage = 'publishRealtimeChannelMessage';
+    static const String onRealtimeConnectionStateChanged = 'onRealtimeConnectionStateChanged';
+    static const String onRealtimeChannelStateChanged = 'onRealtimeChannelStateChanged';
+    static const String onRealtimeChannelMessage = 'onRealtimeChannelMessage';
 }
 
 class TxAblyMessage {
@@ -56,40 +56,40 @@ class TxErrorInfo {
     static const String cause = "cause";
 }
 
-class TxClientOptions{
-	static const String authUrl = "authUrl";
-	static const String authMethod = "authMethod";
-	static const String key = "key";
-	static const String tokenDetails = "tokenDetails";
-	static const String authHeaders = "authHeaders";
-	static const String authParams = "authParams";
-	static const String queryTime = "queryTime";
-	static const String useTokenAuth = "useTokenAuth";
-	static const String hasAuthCallback = "hasAuthCallback";
-	static const String clientId = "clientId";
-	static const String logLevel = "logLevel";
-	static const String tls = "tls";
-	static const String restHost = "restHost";
-	static const String realtimeHost = "realtimeHost";
-	static const String port = "port";
-	static const String tlsPort = "tlsPort";
-	static const String autoConnect = "autoConnect";
-	static const String useBinaryProtocol = "useBinaryProtocol";
-	static const String queueMessages = "queueMessages";
-	static const String echoMessages = "echoMessages";
-	static const String recover = "recover";
-	static const String environment = "environment";
-	static const String idempotentRestPublishing = "idempotentRestPublishing";
-	static const String httpOpenTimeout = "httpOpenTimeout";
-	static const String httpRequestTimeout = "httpRequestTimeout";
-	static const String httpMaxRetryCount = "httpMaxRetryCount";
-	static const String realtimeRequestTimeout = "realtimeRequestTimeout";
-	static const String fallbackHosts = "fallbackHosts";
-	static const String fallbackHostsUseDefault = "fallbackHostsUseDefault";
-	static const String fallbackRetryTimeout = "fallbackRetryTimeout";
-	static const String defaultTokenParams = "defaultTokenParams";
-	static const String channelRetryTimeout = "channelRetryTimeout";
-	static const String transportParams = "transportParams";
+class TxClientOptions {
+    static const String authUrl = "authUrl";
+    static const String authMethod = "authMethod";
+    static const String key = "key";
+    static const String tokenDetails = "tokenDetails";
+    static const String authHeaders = "authHeaders";
+    static const String authParams = "authParams";
+    static const String queryTime = "queryTime";
+    static const String useTokenAuth = "useTokenAuth";
+    static const String hasAuthCallback = "hasAuthCallback";
+    static const String clientId = "clientId";
+    static const String logLevel = "logLevel";
+    static const String tls = "tls";
+    static const String restHost = "restHost";
+    static const String realtimeHost = "realtimeHost";
+    static const String port = "port";
+    static const String tlsPort = "tlsPort";
+    static const String autoConnect = "autoConnect";
+    static const String useBinaryProtocol = "useBinaryProtocol";
+    static const String queueMessages = "queueMessages";
+    static const String echoMessages = "echoMessages";
+    static const String recover = "recover";
+    static const String environment = "environment";
+    static const String idempotentRestPublishing = "idempotentRestPublishing";
+    static const String httpOpenTimeout = "httpOpenTimeout";
+    static const String httpRequestTimeout = "httpRequestTimeout";
+    static const String httpMaxRetryCount = "httpMaxRetryCount";
+    static const String realtimeRequestTimeout = "realtimeRequestTimeout";
+    static const String fallbackHosts = "fallbackHosts";
+    static const String fallbackHostsUseDefault = "fallbackHostsUseDefault";
+    static const String fallbackRetryTimeout = "fallbackRetryTimeout";
+    static const String defaultTokenParams = "defaultTokenParams";
+    static const String channelRetryTimeout = "channelRetryTimeout";
+    static const String transportParams = "transportParams";
 }
 
 class TxTokenDetails {
@@ -108,14 +108,14 @@ class TxTokenParams {
     static const String ttl = "ttl";
 }
 
-class TxTokenRequest{
-	static const String capability = "capability";
-	static const String clientId = "clientId";
-	static const String keyName = "keyName";
-	static const String mac = "mac";
-	static const String nonce = "nonce";
-	static const String timestamp = "timestamp";
-	static const String ttl = "ttl";
+class TxTokenRequest {
+    static const String capability = "capability";
+    static const String clientId = "clientId";
+    static const String keyName = "keyName";
+    static const String mac = "mac";
+    static const String nonce = "nonce";
+    static const String timestamp = "timestamp";
+    static const String ttl = "ttl";
 }
 
 class TxConnectionStateChange {
