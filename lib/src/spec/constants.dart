ec5a561dd4a234b6a9938ccb13ad031ddc80e1bb
Rohit R. Abbadi <rohit@flackon.com>
2020-08-25 19:47:35 +0530

    Introducing ErrorCodes constants

--- lib/src/spec/constants.dart
+++ lib/src/spec/constants.dart
@@ -6,3 +6,12 @@ class LogLevel{
   static const int warn = 5;
   static const int error = 6;
 }
+
+class ErrorCodes {
+  /// error code sent from platform in case if response from
+  /// authCallback is not of valid type.
+  /// When this is encountered, flutter side will re-try the
+  /// method call after responding to authCallback method channel
+  /// call triggered from platform side
+  static const int authCallbackFailure = 80019;
+}
