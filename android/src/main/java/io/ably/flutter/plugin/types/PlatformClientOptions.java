97fb40d115f46ddc3841a64c4465584fc376141a
Rohit R. Abbadi <rohit@flackon.com>
2020-09-11 21:36:21 +0530

    PlatformClientOptions is used as a composition instead of extended class

--- android/src/main/java/io/ably/flutter/plugin/types/PlatformClientOptions.java
+++ android/src/main/java/io/ably/flutter/plugin/types/PlatformClientOptions.java
@@ -2,13 +2,19 @@ package io.ably.flutter.plugin.types;
 
 import io.ably.lib.types.ClientOptions;
 
-public class PlatformClientOptions extends ClientOptions {
+public class PlatformClientOptions {
 
     /**
      * Whether dart side has authCallback.
      * If true, autCallback proxy will be added in methodCallHandler
      * while creating rest/realtime instances
      */
-    public boolean hasAuthCallback;
+    public final boolean hasAuthCallback;
+    public final ClientOptions options;
+
+    public PlatformClientOptions(ClientOptions options, boolean hasAuthCallback) {
+        this.options = options;
+        this.hasAuthCallback = hasAuthCallback;
+    }
 
 }
