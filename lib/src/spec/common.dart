8e29a7948b91108209d7d20f75e978e7fba9a8d3
Rohit R. Abbadi <rohit@flackon.com>
2020-10-07 15:18:20 +0530

    improvise documentation

--- lib/src/spec/common.dart
+++ lib/src/spec/common.dart
@@ -262,13 +262,13 @@ class TokenRequest {
 ///
 /// https://docs.ably.io/client-lib-development-guide/features/#RSL2b
 class RestHistoryParams {
-  /// start must be equal to or less than end and is unaffected
+  /// [start] must be equal to or less than end and is unaffected
   /// by the request direction
   ///
   /// RLS2b1
   final DateTime start;
 
-  /// end must be equal to or greater than start and is unaffected
+  /// [end] must be equal to or greater than start and is unaffected
   /// by the request direction
   ///
   /// RLS2b1
@@ -282,7 +282,7 @@ class RestHistoryParams {
 
   /// Number of items returned in one page
   ///
-  /// limit supports up to 1,000 items.
+  /// [limit] supports up to 1,000 items.
   /// if omitted the direction defaults to the REST API default (100)
   /// RLS2b3
   final int limit;
