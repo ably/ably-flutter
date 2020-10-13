9812d921f8e92848082d0dfceb7fe5ded8a8e046
Guenter Zoechbauer <guenter@gzoechbauer.com>
2020-10-13 15:43:40 +0200

    Try to run all tests at once again

--- test_integration/run_integration_tests.sh
+++ test_integration/run_integration_tests.sh
@@ -5,8 +5,8 @@ set -e
 # It only executes the first it finds
 # flutter drive test_driver/*
 
-flutter drive test_driver/app.dart
-flutter drive test_driver/realtime.dart
-flutter drive test_driver/rest_publish.dart
-flutter drive test_driver/test_helper.dart
-flutter drive test_driver/without_wifi.dart
+flutter drive test_driver/all.dart
+#flutter drive test_driver/realtime.dart
+#flutter drive test_driver/rest_publish.dart
+#flutter drive test_driver/test_helper.dart
+#flutter drive test_driver/without_wifi.dart
