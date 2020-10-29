95b436607736a4fce505a63992f8c66b5cd238db
Rohit R. Abbadi <rohit@flackon.com>
2020-10-30 00:13:27 +0530

    enhancing rest and realtime channel publish tests to test all types of payloads

--- test_integration/lib/test/rest_publish_test.dart
+++ test_integration/lib/test/rest_publish_test.dart
@@ -35,13 +35,22 @@ class RestPublishTestState extends State<RestPublishTest> {
     );
 
     final name = 'Hello';
-    final data = 'Flutter';
+    final messageData = [
+      null,   //null
+      'Ably', //string
+      // TODO(tiholic) test these after implementing Message(s) for rest publish also check why we are unable to pass lists and maps as data directly
+      [1, 2, 3],  //numeric list
+      ['hello', 'ably'], //string list
+      {'hello': 'ably', 'items': ['1', 2.2, '3 thousand']},  //map
+      [{'hello': 'ably'}, 'ably', 'rest']
+    ];
 
     final channel = rest.channels.get('test');
-    await channel.publish(name: name, data: data);
-    await channel.publish(name: name);
-    await channel.publish(data: data);
-    await channel.publish();
+    await channel.publish();  //publish without name and data
+    await channel.publish(data: messageData[1]);  //publish without name
+    for(var data in messageData){
+      await channel.publish(name: name, data: data);
+    }
 
     widget.dispatcher.reportTestCompletion(<String, dynamic>{
       'handle': await rest.handle,
