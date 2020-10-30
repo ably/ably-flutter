31d5b579fb27818abc418a4d0c58ff37dce17ad4
Rohit R. Abbadi <rohit@flackon.com>
2020-10-30 12:57:09 +0530

    integration tests for realtime subscribe with filters

--- test_integration/test_driver/test_implementation/realtime_tests.dart
+++ test_integration/test_driver/test_implementation/realtime_tests.dart
@@ -57,36 +57,86 @@ Future testRealtimeSubscribe(FlutterDriver driver) async {
 
   expect(response.testName, message.testName);
 
-  List<Map<String, dynamic>> messages = response.payload['messages']
+  // Testing realtime subscribe to all messages
+  List<Map<String, dynamic>> all = response.payload['all']
       .map<Map<String, dynamic>>(
           (m) => Map.castFrom<dynamic, dynamic, String, dynamic>(m))
       .toList();
 
-  expect(messages, isA<List<Map<String, dynamic>>>());
-  expect(messages.length, equals(8));
+  expect(all, isA<List<Map<String, dynamic>>>());
+  expect(all.length, equals(8));
 
-  expect(messages[0]['name'], isNull);
-  expect(messages[0]['data'], isNull);
+  expect(all[0]['name'], isNull);
+  expect(all[0]['data'], isNull);
 
-  expect(messages[1]['name'], isNull);
-  expect(messages[1]['data'], equals('Ably'));
+  expect(all[1]['name'], isNull);
+  expect(all[1]['data'], equals('Ably'));
 
-  expect(messages[2]['name'], 'Hello');
-  expect(messages[2]['data'], isNull);
-  expect(messages[3]['data'], equals('Ably'));
-  expect(messages[4]['data'], equals([1, 2, 3]));
-  expect(messages[5]['data'], equals(['hello', 'ably']));
+  expect(all[2]['name'], 'name1');
+  expect(all[2]['data'], isNull);
+
+  expect(all[3]['name'], 'name1');
+  expect(all[3]['data'], equals('Ably'));
+
+  expect(all[4]['name'], 'name2');
+  expect(all[4]['data'], equals([1, 2, 3]));
+
+  expect(all[5]['name'], 'name2');
+  expect(all[5]['data'], equals(['hello', 'ably']));
+
+  expect(all[6]['name'], 'name3');
   expect(
-      messages[6]['data'],
+      all[6]['data'],
       equals({
         'hello': 'ably',
         'items': ['1', 2.2, true]
       }));
+
+  expect(all[7]['name'], 'name3');
   expect(
-      messages[7]['data'],
+      all[7]['data'],
       equals([
         {'hello': 'ably'},
         'ably',
         'realtime'
       ]));
+
+  // Testing realtime subscribe to messages filtered with name
+  List<Map<String, dynamic>> filteredWithName = response
+      .payload['filteredWithName']
+      .map<Map<String, dynamic>>(
+          (m) => Map.castFrom<dynamic, dynamic, String, dynamic>(m))
+      .toList();
+
+  expect(filteredWithName, isA<List<Map<String, dynamic>>>());
+  expect(filteredWithName.length, equals(2));
+
+  expect(filteredWithName[0]['name'], 'name1');
+  expect(filteredWithName[0]['data'], isNull);
+
+  expect(filteredWithName[1]['name'], 'name1');
+  expect(filteredWithName[1]['data'], equals('Ably'));
+
+  // Testing realtime subscribe to messages filtered with multiple names
+  List<Map<String, dynamic>> filteredWithNames = response
+      .payload['filteredWithNames']
+      .map<Map<String, dynamic>>(
+          (m) => Map.castFrom<dynamic, dynamic, String, dynamic>(m))
+      .toList();
+
+  expect(filteredWithNames, isA<List<Map<String, dynamic>>>());
+  expect(filteredWithNames.length, equals(4));
+
+  expect(filteredWithNames[0]['name'], 'name1');
+  expect(filteredWithNames[0]['data'], isNull);
+
+  expect(filteredWithNames[1]['name'], 'name1');
+  expect(filteredWithNames[1]['data'], equals('Ably'));
+
+  expect(filteredWithNames[2]['name'], 'name2');
+  expect(filteredWithNames[2]['data'], equals([1, 2, 3]));
+
+  expect(filteredWithNames[3]['name'], 'name2');
+  expect(filteredWithNames[3]['data'], equals(['hello', 'ably']));
+
 }
