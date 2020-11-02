baeb576905a3bed6f1256169371d64e601b2e038
Rohit R. Abbadi <rohit@flackon.com>
2020-11-02 14:20:39 +0530

    cleaning up run_integration_tests.sh

--- test_integration/run_integration_tests.sh
+++ test_integration/run_integration_tests.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 
 set -e
-# TODO(tiholic) for some unknown reason this does not execute all tests
-# It only executes the first it finds
-# flutter drive test_driver/*
-
-flutter drive test_driver/all.dart
-#flutter drive test_driver/realtime.dart
-#flutter drive test_driver/rest_publish.dart
-#flutter drive test_driver/test_helper.dart
-#flutter drive test_driver/without_wifi.dart
+flutter drive test_driver/*
