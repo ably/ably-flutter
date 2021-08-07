b1a7e4c21a1267f5dd57547a0f3680affbc875fb
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2021-08-07 09:45:16 +0100

    Support more than iOS and Android strings (future proofing for other platforms)

--- example/lib/constants.dart
+++ example/lib/constants.dart
@@ -1,8 +1,11 @@
 import 'dart:io';
 
+import 'package:enum_to_string/enum_to_string.dart';
+import 'package:flutter/foundation.dart';
+
 class Constants {
   static const String ablyApiKey = 'ABLY_API_KEY';
-  static String platform = (Platform.isAndroid) ? 'android' : 'ios';
+  static String platform = EnumToString.convertToString(defaultTargetPlatform);
   static String clientId = 'ably-flutter-example-app-$platform-client-id';
   static const String channelName = 'test-channel';
   static const String channelNameForPushNotifications =
