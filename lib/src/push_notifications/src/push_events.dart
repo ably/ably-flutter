491f811070f5b11f4bd2bca60653174dd57b6d1a
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2021-08-26 06:24:41 +0100

    Remove unused code and commented code (will be re-added for push notification listeners)

--- lib/src/push_notifications/src/push_events.dart
+++ lib/src/push_notifications/src/push_events.dart
@@ -25,21 +25,4 @@ abstract class PushEvents {
   /// way you will know that a new APNs/ FCM token provided by the OS
   /// fails to be sent to Ably.
   Stream<ErrorInfo> get onUpdateFailed;
-
-  // TODO implement message handlers: https://github.com/ably/ably-flutter/issues/141
-  // /// Returns a stream which emits a value everytime a message is received
-  // /// whilst the app is in the foreground
-  // Stream<PushMessage> get onMessage;
-  //
-  // /// Returns a future which resolves when a message is received whilst
-  // /// the application is in the background or terminated.
-  // Future<PushMessage> get onBackgroundMessage;
-  //
-  // /// Immediately returns a future which resolves to a message the app launch
-  // /// was the result of tapping a notification.
-  // Future<PushMessage> get onNotificationMessageTapLaunchesApp;
-  //
-  // /// Returns a stream which emits a value when a notification is tapped when
-  // /// the app is either in the foreground or background, but not terminated.
-  // Stream<PushMessage> get onNotificationMessageTapResumesApp;
 }
--- lib/src/push_notifications/src/push_message.dart
+++
@@ -1,3 +0,0 @@
-/// A class representing a userInfo dictionary available from iOS/ APNs
-/// and RemoteMessage from Android/ FCM.
-class PushMessage {}
