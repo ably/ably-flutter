f7df4f3c0f7a81856b223fe944125595bf6d30b0
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2021-09-14 13:05:00 +0100

    Set Add warning about setting UNUserNotificationCenter.delegate and Re-indent App delegate using Xcode

--- example/ios/Runner/AppDelegate.m
+++ example/ios/Runner/AppDelegate.m
@@ -4,11 +4,15 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
-    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
-    UNUserNotificationCenter.currentNotificationCenter.delegate = self;
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Warning: Do not set UNUserNotificationCenter.currentNotificationCenter.delegate
+    // AFTER GeneratedPluginRegistrant:registerWithRegistry: is called (when Ably Flutter
+    // initializes), as this would break functionality. Instead, set it before plugin
+    // registration:
+    //UNUserNotificationCenter.currentNotificationCenter.delegate = self;
+    [GeneratedPluginRegistrant registerWithRegistry:self];
+    // Override point for customization after application launch.
+    return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
 - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
