f067867c92635a7e2d597fcf2e4cf897afbb2a5f
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2021-09-16 20:02:39 +0100

    And interfaces in Java for callbacks

--- android/src/main/java/io/ably/flutter/plugin/push/CompletionHandler.java
+++ android/src/main/java/io/ably/flutter/plugin/push/CompletionHandler.java
@@ -2,6 +2,7 @@ package io.ably.flutter.plugin.push;
 
 import com.google.firebase.messaging.RemoteMessage;
 
-public interface CompletionHandlerWithRemoteMessage {
-  void call(RemoteMessage message);
+interface CompletionHandler {
+  void call();
 }
+
