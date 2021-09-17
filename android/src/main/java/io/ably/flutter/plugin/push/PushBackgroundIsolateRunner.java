c8d7108849adeac9bc03232278e69a5bdd654164
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2021-09-17 21:14:20 +0100

    Simplify FlutterEngine creation by removing automatically called code

--- android/src/main/java/io/ably/flutter/plugin/push/PushBackgroundIsolateRunner.java
+++ android/src/main/java/io/ably/flutter/plugin/push/PushBackgroundIsolateRunner.java
@@ -38,17 +38,12 @@ public class PushBackgroundIsolateRunner implements MethodChannel.MethodCallHand
   public PushBackgroundIsolateRunner(Context context, BroadcastReceiver.PendingResult asyncCompletionHandlerPendingResult, RemoteMessage message) {
     this.asyncCompletionHandlerPendingResult = asyncCompletionHandlerPendingResult;
     this.remoteMessage = message;
-
-    FlutterLoader flutterLoader = FlutterInjector.instance().flutterLoader();
-    flutterLoader.startInitialization(context); // Starts initialization when Dart VM is not yet running, e.g. The main app is terminated.
-    flutterLoader.ensureInitializationComplete(context, null); // Blocks the thread so we can be sure the future methods will be fine. This is probably optional.
     flutterEngine = new FlutterEngine(context, null);
     DartExecutor executor = flutterEngine.getDartExecutor();
     backgroundMethodChannel = new MethodChannel(executor.getBinaryMessenger(), "io.ably.flutter.plugin.background", new StandardMethodCodec(new AblyMessageCodec()));
     backgroundMethodChannel.setMethodCallHandler(this);
     // Get and launch the users app isolate manually:
-    DartExecutor.DartEntrypoint appEntrypoint = DartExecutor.DartEntrypoint.createDefault();
-    executor.executeDartEntrypoint(appEntrypoint);
+    executor.executeDartEntrypoint(DartExecutor.DartEntrypoint.createDefault());
   }
 
   /**
