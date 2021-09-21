09fd9cd386ef7bed28f3bef578b2da01989c7631
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2021-09-21 16:32:29 +0100

    Rename call to onRemoteMessage

--- android/src/main/java/io/ably/flutter/plugin/push/CompletionHandlerWithRemoteMessage.java
+++ android/src/main/java/io/ably/flutter/plugin/push/CompletionHandlerWithRemoteMessage.java
@@ -3,5 +3,5 @@ package io.ably.flutter.plugin.push;
 import com.google.firebase.messaging.RemoteMessage;
 
 public interface CompletionHandlerWithRemoteMessage {
-  void call(RemoteMessage message);
+  void onRemoteMessage(RemoteMessage message);
 }
