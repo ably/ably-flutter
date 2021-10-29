c73d420913a063d2ff1563c2e1acb7b8d4793ef4
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2021-10-29 15:29:42 +0100

    Make cipher an optional constructor parameter, so users are not forced to set it

--- lib/src/rest/src/channels.dart
+++ lib/src/rest/src/channels.dart
@@ -3,7 +3,7 @@ import '../../common/src/channels.dart';
 import '../../message/src/message.dart';
 import '../../push_notifications/push_notifications.dart';
 import '../rest.dart';
-import 'channel_options.dart';
+import 'rest_channel_options.dart';
 import 'rest.dart';
 
 /// A named channel through with rest client can interact with ably service.
--- lib/src/rest/src/rest_channel_options.dart
+++ lib/src/rest/src/rest_channel_options.dart
@@ -1,10 +1,12 @@
+import 'package:ably_flutter/src/authentication/authentication.dart';
+
 /// options provided when instantiating a channel
 ///
 /// https://docs.ably.com/client-lib-development-guide/features/#TB1
 class RestChannelOptions {
   /// https://docs.ably.com/client-lib-development-guide/features/#TB2b
-  final Object cipher;
+  final CipherParams? cipher;
 
   /// create channel options with a cipher
-  RestChannelOptions(this.cipher);
+  RestChannelOptions({this.cipher});
 }
