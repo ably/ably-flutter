601cf063638a06d5a0f93272e5cf541740e905d2
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2021-11-16 16:11:30 +0000

    Add toggle for UI instead of 2 buttons

--- example/lib/ui/message_encryption/message_encryption_sliver.dart
+++ example/lib/ui/message_encryption/message_encryption_sliver.dart
@@ -1,101 +1,121 @@
 import 'package:ably_flutter/ably_flutter.dart' as ably;
+import 'package:ably_flutter_example/ui/text_with_label.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 import '../../encrypted_messaging_service.dart';
 
-class MessageEncryptionSliver extends StatelessWidget {
+class MessageEncryptionSliver extends HookWidget {
   EncryptedMessagingService? encryptedMessagingService;
 
   MessageEncryptionSliver(this.encryptedMessagingService, {Key? key})
       : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const Text(
-            'Message Encryption',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(vertical: 8.0),
-            child: Text(
-                'Channel name: "${EncryptedMessagingService.channelName}"'),
-          ),
-          const Text(
-            'Publish encrypted message using:',
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              TextButton(
-                child: const Text('Rest client'),
-                onPressed: () {
-                  encryptedMessagingService?.publishRestMessage(
-                      'Hello', {'payload': 'this should be encrypted'});
-                },
-              ),
-              TextButton(
-                onPressed: () async {
-                  await encryptedMessagingService?.publishRealtimeMessage(
-                      'Hello', {'payload': 'this should be encrypted'});
-                },
-                child: const Text('Realtime client'),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Text(
-                'Recent messages:',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              IconButton(
-                  onPressed: encryptedMessagingService?.clearMessageHistory,
-                  icon: const Icon(Icons.delete))
-            ],
-          ),
-          TextButton(
-            onPressed: encryptedMessagingService?.logChannelMessages,
-            child: const Text('Log channel messages'),
-          ),
-          TextButton(
-              onPressed: encryptedMessagingService?.unsubscribeAndDetach,
-              child: const Text('Unsubscribe and detach from channel')),
-          StreamBuilder<List<ably.Message>>(
-            stream: encryptedMessagingService?.messageHistoryStream,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                final messages = snapshot.data as List<ably.Message>;
-                if (messages.isEmpty) {
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: const Text('No messages yet'),
-                  );
-                }
+  Widget build(BuildContext context) {
+    final logMessages = useState(false);
 
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const Text(
+          'Message Encryption',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        const Padding(
+          padding: EdgeInsets.symmetric(vertical: 8.0),
+          child:
+              Text('Channel name: "${EncryptedMessagingService.channelName}"'),
+        ),
+        const Text(
+          'Publish encrypted message using:',
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            TextButton(
+              child: const Text('Rest client'),
+              onPressed: () {
+                encryptedMessagingService?.publishRestMessage(
+                    'Encrypted message',
+                    {'payload': 'this should be encrypted'});
+              },
+            ),
+            TextButton(
+              onPressed: () async {
+                await encryptedMessagingService?.publishRealtimeMessage(
+                    'Encrypted message',
+                    {'payload': 'this should be encrypted'});
+              },
+              child: const Text('Realtime client'),
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              'Log recent messages',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            Switch(
+              onChanged: (switchedOn) {
+                logMessages.value = switchedOn;
+                if (switchedOn) {
+                  encryptedMessagingService?.logChannelMessages();
+                } else {
+                  encryptedMessagingService?.unsubscribeAndDetach();
+                }
+              },
+              value: logMessages.value,
+            ),
+            IconButton(
+                onPressed: encryptedMessagingService?.clearMessageHistory,
+                icon: const Icon(Icons.delete))
+          ],
+        ),
+        StreamBuilder<List<ably.Message>>(
+          stream: encryptedMessagingService?.messageHistoryStream,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              final messages = snapshot.data as List<ably.Message>;
+              if (messages.isNotEmpty) {
                 return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: messages
-                      .map((message) => Row(
-                            children: [
-                              Text((message.name != null)
-                                  ? message.name.toString()
-                                  : 'NO NAME'),
-                              Text((message.data != null)
-                                  ? message.data.toString()
-                                  : 'NO DATA'),
-                            ],
-                          ))
+                      .map((message) =>
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 4),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            TextWithLabel(
+                                'Name',
+                                (message.name != null)
+                                    ? message.name.toString()
+                                    : 'NO NAME'),
+                            TextWithLabel(
+                                'Data',
+                                (message.data != null)
+                                    ? message.data.toString()
+                                    : 'NO DATA'),
+                          ],
+                        ),
+                      ))
                       .toList(),
                 );
+              } else if (!logMessages.value) {
+                return const Text('Enable logging to receive messages');
               } else {
-                return Text('No messages yet');
+                return const Text('No messages yet');
               }
-            },
-          ),
-        ],
-      );
+            }
+            return const Text('No data yet');
+          },
+        ),
+      ],
+    );
+  }
 }
