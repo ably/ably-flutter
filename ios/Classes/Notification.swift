a2be8e867f9876982706efd9fb76b508dd6bcf18
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2021-11-18 10:03:46 +0000

    Apply suggestions from code review
    Use `[AnyHashable: Any]` instead of `NSDictionary`
    
    Co-authored-by: Łukasz Szyszkowski <47628502+lukasz-szyszkowski@users.noreply.github.com>

--- ios/Classes/Notification.swift
+++ ios/Classes/Notification.swift
@@ -12,8 +12,8 @@ class Notification: NSObject {
     }
     
     convenience init?(from userInfo: [AnyHashable : Any]) {
-        if let aps = userInfo["aps"] as? NSDictionary {
-            if let alert = aps["alert"] as? NSDictionary {
+        if let aps = userInfo["aps"] as? [AnyHashable: Any] {
+            if let alert = aps["alert"] as? [AnyHashable: Any] {
                 let title = alert["title"] as? String
                 let body = alert["body"] as? String
                 self.init(title: title, body: body)
