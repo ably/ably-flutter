61bbbe22601998faf08b070a88ec92ba78376d41
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2021-11-18 09:44:04 +0000

    Handle `aps["alert"]` being a String instead of a NSDictionary

--- ios/Classes/Notification.swift
+++ ios/Classes/Notification.swift
@@ -6,25 +6,26 @@ class Notification: NSObject {
     @objc
     var body: String?;
     
-    init(title: String, body: String) {
-        self.title = title;
-        self.body = body;
-    }
-    
-    override init() {
-        self.title = nil;
-        self.body = nil;
+    init(title: String?, body: String?) {
+      self.title = title
+      self.body = body
     }
     
     convenience init?(from userInfo: [AnyHashable : Any]) {
-        self.init()
         if let aps = userInfo["aps"] as? NSDictionary {
             if let alert = aps["alert"] as? NSDictionary {
-                title = alert["title"] as? String
-                body = alert["body"] as? String
+                let title = alert["title"] as? String
+                let body = alert["body"] as? String
+                self.init(title: title, body: body)
+            } else if let title = aps["alert"] as? String {
+                self.init(title: title, body: nil)
             } else {
-                return nil
+                self.init(title: nil, body: nil)
+                return nil;
             }
+        } else {
+            self.init(title: nil, body: nil)
+            return nil;
         }
     }
 }
