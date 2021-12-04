ba7f505c6c35c3b250c34c8dbdf98a965d348603
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2021-12-04 15:32:28 +0000

    Replace emojis with simpler messages

--- format-files.sh
+++ format-files.sh
@@ -6,11 +6,11 @@ MY_PATH=$(dirname "$0")
 
 . format-files-dependencies.sh
 
-echo "Formatting Dart 🐣 files..."
+echo "Formatting Dart files..."
 flutter format $MY_PATH
-echo "Formatted Dart 🐥 files.\n"
+echo "  ✅ Formatted Dart files.\n"
 
-echo "Formatting Java 🌰 files..."
+echo "Formatting Java files..."
 JAVA_FILES=$(find $MY_PATH/android -name "*java" -type f -exec ls {} \;)
 echo $JAVA_FILES | xargs java \
   --add-exports jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED \
@@ -19,9 +19,9 @@ echo $JAVA_FILES | xargs java \
   --add-exports jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED \
   --add-exports jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED \
   -jar $GOOGLE_JAVA_FORMAT_PATH --replace
-echo "Formatted Java ☕️ files.\n"
+echo "  ✅ Formatted Java files.\n"
 
-echo "Formatting Swift 💨 files..."
+echo "Formatting Swift files..."
 SWIFT_VERSION=5.5
 swiftformat --swiftversion $SWIFT_VERSION $MY_PATH
-echo "Formatted Swift 🌬 files.\n"
+echo "  ✅ Formatted Swift files.\n"
