4233df4778da6d7f1b5642851b70b39163383bc3
Igor Kurek <ikurek@users.noreply.github.com>
2022-04-05 16:16:35 +0200

    Created a mixin for calculating hashcodes

---
+++ lib/src/common/src/object_hash.dart
@@ -0,0 +1,16 @@
+/// Utility mixin to calculate hashCode values for complex objects
+mixin ObjectHash {
+  /// Returns hashCode value for list of fields inside and object
+  /// that uses the mixin. Hash value is calculated with a pretty standard hash
+  /// algorithm, using 31 as a prime number and adding 0 for null values
+  /// This is roughly based on default JDK implementations
+  int objectHash(List<dynamic> fields) {
+    var result = 1;
+
+    for (final field in fields) {
+      result = 31 * result + (field == null ? 0 : field.hashCode);
+    }
+
+    return result;
+  }
+}
