f1d26ae65073d778529b4f0bc032b22172e4eb5a
Igor Kurek <ikurek@users.noreply.github.com>
2022-04-06 14:11:43 +0200

    Sorted constructor parameters in message

--- lib/src/message/src/delta_extras.dart
+++ lib/src/message/src/delta_extras.dart
@@ -14,8 +14,8 @@ class DeltaExtras {
   /// create instance from a map
   @protected
   DeltaExtras.fromMap(Map value)
-      : from = value[TxDeltaExtras.from] as String?,
-        format = value[TxDeltaExtras.format] as String?;
+      : format = value[TxDeltaExtras.format] as String?,
+        from = value[TxDeltaExtras.from] as String?;
 
   @override
   bool operator ==(Object other) =>
