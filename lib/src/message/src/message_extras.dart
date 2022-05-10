bbffaa136d0adeb90d7ae0d7c0fe2d4f445ebe29
Igor Kurek <ikurek@users.noreply.github.com>
2022-05-10 10:37:52 +0200

    Removed unnecessary import from MessageExtras

--- lib/src/message/src/message_extras.dart
+++ lib/src/message/src/message_extras.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:ably_flutter/ably_flutter.dart';
 import 'package:ably_flutter/src/common/src/object_hash.dart';
 import 'package:ably_flutter/src/platform/platform_internal.dart';
