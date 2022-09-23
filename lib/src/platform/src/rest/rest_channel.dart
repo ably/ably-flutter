82645e95829f3741fcb5b639acb85b7bbfe141f5
Jakub Jankowski <j.jankowski@nomtek.com>
2022-09-23 11:54:21 +0200

    Add canonical docstring for RestChannel#history
    This is taken verbatim from the repo
    ably/canonical-api-reference-prototyping at commit 6296da4

--- lib/src/platform/src/rest/rest_channel.dart
+++ lib/src/platform/src/rest/rest_channel.dart
@@ -69,6 +69,28 @@ class RestChannel extends PlatformObject {
   ///
   /// https://docs.ably.com/client-lib-development-guide/features/#RSL2
   /// END LEGACY DOCSTRING
+
+  /// BEGIN CANONICAL DOCSTRING
+  /// Retrieves a [PaginatedResult]{@link PaginatedResult} object, containing an
+  /// array of historical [Message]{@link Message} objects for the channel.
+  /// If the channel is configured to persist messages, then messages can be
+  /// retrieved from history for up to 72 hours in the past. If not, messages
+  /// can only be retrieved from history for up to two minutes in the past.
+  ///
+  /// [start] - The time from which messages are retrieved, specified as
+  /// milliseconds since the Unix epoch.
+  /// [end] - The time until messages are retrieved, specified as milliseconds
+  /// since the Unix epoch.
+  /// [direction] - The order for which messages are returned in. Valid values
+  /// are backwards which orders messages from most recent to oldest, or
+  /// forwards which orders messages from oldest to most recent. The default is
+  /// backwards.
+  /// [limit] - An upper limit on the number of messages returned. The default
+  /// is 100, and the maximum is 1000.
+  ///
+  /// [PaginatedResult<Message>] - A [PaginatedResult]{@link PaginatedResult}
+  /// object containing an array of [Message]{@link Message} objects.
+  /// END CANONICAL DOCSTRING
   Future<PaginatedResult<Message>> history([
     RestHistoryParams? params,
   ]) async {
