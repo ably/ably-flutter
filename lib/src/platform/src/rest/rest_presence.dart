c89f56c43d2a6335eba889d0f106fbd9a344786d
Jakub Jankowski <j.jankowski@nomtek.com>
2022-09-24 19:47:43 +0200

    Add canonical docstring for RestPresence#history
    This is taken verbatim from the repo
    ably/canonical-api-reference-prototyping at commit 6296da4

--- lib/src/platform/src/rest/rest_presence.dart
+++ lib/src/platform/src/rest/rest_presence.dart
@@ -66,6 +66,30 @@ class RestPresence extends PlatformObject {
   ///
   /// https://docs.ably.com/client-lib-development-guide/features/#RSP4
   /// END LEGACY DOCSTRING
+
+  /// BEGIN CANONICAL DOCSTRING
+  /// Retrieves a [PaginatedResult]{@link PaginatedResult} object, containing an
+  /// array of historical [PresenceMessage]{@link PresenceMessage} objects for
+  /// the channel. If the channel is configured to persist messages, then
+  /// presence messages can be retrieved from history for up to 72 hours in the
+  /// past. If not, presence messages can only be retrieved from history for up
+  /// to two minutes in the past.
+  ///
+  /// [start] - The time from which messages are retrieved, specified as
+  /// milliseconds since the Unix epoch.
+  /// [end] - The time until messages are retrieved, specified as milliseconds
+  /// since the Unix epoch.
+  /// [direction] - The order for which messages are returned in. Valid values
+  /// are backwards which orders messages from most recent to oldest, or
+  /// forwards which orders messages from oldest to most recent. The default is
+  /// backwards.
+  /// [limit] - An upper limit on the number of messages returned. The default
+  /// is 100, and the maximum is 1000.
+  ///
+  /// [PaginatedResult<PresenceMessage>] - 	A
+  /// [PaginatedResult]{@link PaginatedResult} object containing an array of
+  /// [PresenceMessage]{@link PresenceMessage} objects.
+  /// END CANONICAL DOCSTRING
   Future<PaginatedResult<PresenceMessage>> history([
     RestHistoryParams? params,
   ]) async {
