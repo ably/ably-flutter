f789cb429eb6ac31ba18ee6934b84963d099c3b4
Jakub Jankowski <j.jankowski@nomtek.com>
2022-09-26 01:54:28 +0200

    Add canonical docstring for class DeltaExtras

--- lib/src/message/src/delta_extras.dart
+++ lib/src/message/src/delta_extras.dart
@@ -6,16 +6,30 @@ import 'package:meta/meta.dart';
 /// BEGIN LEGACY DOCSTRING
 /// Delta extension configuration for [MessageExtras]
 /// END LEGACY DOCSTRING
+
+/// BEGIN CANONICAL DOCSTRING
+/// Contains any arbitrary key-value pairs, which may also contain other
+/// primitive JSON types, JSON-encodable objects, or JSON-encodable arrays from
+/// delta compression.
+/// END CANONICAL DOCSTRING
 @immutable
 class DeltaExtras with ObjectHash {
   /// BEGIN LEGACY DOCSTRING
   /// the id of the message the delta was generated from
   /// END LEGACY DOCSTRING
+
+  /// BEGIN CANONICAL DOCSTRING
+  /// The ID of the message the delta was generated from.
+  /// END CANONICAL DOCSTRING
   final String? from;
 
   /// BEGIN LEGACY DOCSTRING
   /// the delta format. Only "vcdiff" is supported currently
   /// END LEGACY DOCSTRING
+
+  /// BEGIN CANONICAL DOCSTRING
+  /// The delta compression format. Only vcdiff is supported.
+  /// END CANONICAL DOCSTRING
   final String? format;
 
   /// BEGIN LEGACY DOCSTRING
