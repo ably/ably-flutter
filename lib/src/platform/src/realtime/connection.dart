38e84f8eaf7c86825a6317cc8e1f9c1cec4f9cbb
Jakub Jankowski <j.jankowski@nomtek.com>
2022-09-25 22:02:09 +0200

    Add canonical docstring for Connection#key
    This is taken verbatim from the repo
    ably/canonical-api-reference-prototyping at commit 6296da4

--- lib/src/platform/src/realtime/connection.dart
+++ lib/src/platform/src/realtime/connection.dart
@@ -71,6 +71,16 @@ class Connection extends PlatformObject {
   ///
   /// https://docs.ably.com/client-lib-development-guide/features/#RTN9
   /// END LEGACY DOCSTRING
+
+  /// BEGIN CANONICAL DOCSTRING
+  /// A unique private connection key used to recover or resume a connection,
+  /// assigned by Ably. When recovering a connection explicitly, the recoveryKey
+  /// is used in the recover client options as it contains both the key and the
+  /// last message serial. This private connection key can also be used by other
+  /// REST clients to publish on behalf of this client. See the
+  /// [publishing over REST on behalf of a realtime client docs](https://ably.com/docs/rest/channels#publish-on-behalf)
+  /// for more info.
+  /// END CANONICAL DOCSTRING
   String? key;
 
   /// BEGIN LEGACY DOCSTRING
