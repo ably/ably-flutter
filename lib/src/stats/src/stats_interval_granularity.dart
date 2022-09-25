8d1e70f5e8b44056d7b1022a3a0b641cd8ec8a4a
Jakub Jankowski <j.jankowski@nomtek.com>
2022-09-25 23:18:15 +0200

    Add canonical docstring for enum StatsIntervalGranularity
    This is taken verbatim from the repo
    ably/canonical-api-reference-prototyping at commit 6296da4

--- lib/src/stats/src/stats_interval_granularity.dart
+++ lib/src/stats/src/stats_interval_granularity.dart
@@ -1,6 +1,10 @@
 /// BEGIN LEGACY DOCSTRING
 /// https://docs.ably.com/client-lib-development-guide/features/#TS12c
 /// END LEGACY DOCSTRING
+
+/// BEGIN CANONICAL DOCSTRING
+/// Describes the interval unit over which statistics are gathered.
+/// END CANONICAL DOCSTRING
 enum StatsIntervalGranularity {
   /// BEGIN LEGACY DOCSTRING
   /// indicates units in minutes
