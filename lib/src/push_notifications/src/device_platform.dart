b700bd931271e86fe20c05b8987af754545fc5ab
Jakub Jankowski <j.jankowski@nomtek.com>
2022-09-26 01:31:56 +0200

    Add canonical docstring for enum DevicePlatform

--- lib/src/push_notifications/src/device_platform.dart
+++ lib/src/push_notifications/src/device_platform.dart
@@ -4,19 +4,35 @@ import 'package:ably_flutter/ably_flutter.dart';
 /// To indicate the operating system -or- platform of the client using SDK
 /// in [DeviceDetails] while registering
 /// END LEGACY DOCSTRING
+
+/// BEGIN CANONICAL DOCSTRING
+/// Describes the device receiving push notifications.
+/// END CANONICAL DOCSTRING
 enum DevicePlatform {
   /// BEGIN LEGACY DOCSTRING
   /// indicates an android device
   /// END LEGACY DOCSTRING
+
+  /// BEGIN CANONICAL DOCSTRING
+  /// The device platform is Android.
+  /// END CANONICAL DOCSTRING
   android,
 
   /// BEGIN LEGACY DOCSTRING
   /// indicates an iOS device
   /// END LEGACY DOCSTRING
+
+  /// BEGIN CANONICAL DOCSTRING
+  /// The device platform is iOS.
+  /// END CANONICAL DOCSTRING
   ios,
 
   /// BEGIN LEGACY DOCSTRING
   /// indicates a browser
   /// END LEGACY DOCSTRING
+
+  /// BEGIN CANONICAL DOCSTRING
+  /// The device platform is a web browser.
+  /// END CANONICAL DOCSTRING
   browser,
 }
