47ebdfe5093b8f62ce3a6b4cf8dffe66da08ebb5
Jakub Jankowski <j.jankowski@nomtek.com>
2022-09-30 10:41:40 +0200

    Edit canonical docstrings for class Rest

--- lib/src/platform/src/rest/rest.dart
+++ lib/src/platform/src/rest/rest.dart
@@ -12,28 +12,23 @@ Map<int?, Rest> _restInstances = {};
 Map<int?, Rest> get restInstances => UnmodifiableMapView(_restInstances);
 
 /// BEGIN LEGACY DOCSTRING
-/// END LEGACY DOCSTRING
 /// A simple stateless API to interact directly with Ably’s REST API
 ///
 /// Learn more at the [REST Client Library API documentation](https://ably.com/documentation/rest).
 /// END LEGACY DOCSTRING
 
-/// BEGIN CANONICAL DOCSTRING
+/// BEGIN EDITED CANONICAL DOCSTRING
 /// A client that offers a simple stateless API to interact directly with Ably's
 /// REST API.
-/// END CANONICAL DOCSTRING
+/// END EDITED CANONICAL DOCSTRING
 class Rest extends PlatformObject {
   /// BEGIN LEGACY DOCSTRING
   /// Pass a [ClientOptions] to configure the rest client.
   /// END LEGACY DOCSTRING
   ///
-  /// BEGIN CANONICAL DOCSTRING
-  /// Construct a RestClient object using an Ably
-  /// [ClientOptions]{@link ClientOptions} object.
-  ///
-  /// [ClientOptions] - A [ClientOptions]{@link ClientOptions} object to
-  /// configure the client connection to Ably.
-  /// END CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
+  /// Construct a [Rest] object using an Ably [options] object.
+  /// END EDITED CANONICAL DOCSTRING
   Rest({required this.options}) : super() {
     channels = RestChannels(this);
     push = Push(rest: this);
@@ -43,12 +38,10 @@ class Rest extends PlatformObject {
   /// Create a rest client from an API key without configuring other parameters
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
-  /// Constructs a RestClient object using an Ably API key or token string.
-  ///
-  /// [keyOrTokenStr] - The Ably API key or token string used to validate the
-  /// client.
-  /// END CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
+  /// Constructs a RestClient object using an Ably API [key] or token string
+  /// that's used to validate the cliet.
+  /// END EDITED CANONICAL DOCSTRING
   factory Rest.fromKey(String key) => Rest(options: ClientOptions(key: key));
 
   @override
@@ -67,9 +60,9 @@ class Rest extends PlatformObject {
   /// https://docs.ably.com/client-lib-development-guide/features/#RSC5
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
-  /// An [Auth]{@link Auth} object.
-  /// END CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
+  /// An [Auth] object.
+  /// END EDITED CANONICAL DOCSTRING
   // Auth? auth;
 
   /// BEGIN LEGACY DOCSTRING
@@ -85,25 +78,23 @@ class Rest extends PlatformObject {
   /// https://docs.ably.com/client-lib-development-guide/features/#RSC19
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
-  /// Makes a REST request to a provided path. This is provided as a convenience
-  /// for developers who wish to use REST API functionality that is either not
-  /// documented or is not yet included in the public API, without having to
-  /// directly handle features such as authentication, paging, fallback hosts,
-  /// MsgPack and JSON support.
-  ///
-  /// [method] - The request method to use, such as GET, POST.
-  /// [path] - The request path.
-  /// [params] - The parameters to include in the URL query of the request.
-  /// The parameters depend on the endpoint being queried. See the REST API
-  /// reference for the available parameters of each endpoint.
-  /// [body] - The JSON body of the request.
-  /// [headers] - Additional HTTP headers to include in the request.
-  ///
-  /// [HttpPaginatedResponse] - An
-  /// [HttpPaginatedResponse]{@link HttpPaginatedResponse} object returned by
+  /// BEGIN EDITED CANONICAL DOCSTRING
+  /// Makes a REST request to a provided [path] using a [method], such as GET,
+  /// POST.
+  /// [params] can be specified to include in the URL query of the
+  /// request. The parameters depend on the endpoint being queried. See the
+  /// [REST API reference](https://ably.com/docs/api/rest-api) for the available
+  /// parameters of each endpoint.
+  /// You can also provide the JSON [body] and additional [headers] to include
+  /// in the request.
+  /// Returns a [HttpPaginatedResponse] object returned by
   /// the HTTP request, containing an empty or JSON-encodable object.
-  /// END CANONICAL DOCSTRING
+  ///
+  /// This is provided as a convenience for developers who wish to use REST API
+  /// functionality that is either not documented or is not yet included in the
+  /// public API, without having to directly handle features such as
+  /// authentication, paging, fallback hosts, MsgPack and JSON support.
+  /// END EDITED CANONICAL DOCSTRING
   // Future<HttpPaginatedResponse> request({
   //   String? method,
   //   String? path,
@@ -120,27 +111,25 @@ class Rest extends PlatformObject {
   /// https://docs.ably.com/client-lib-development-guide/features/#RSC6
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// Queries the REST /stats API and retrieves your application's usage
-  /// statistics. Returns a [PaginatedResult]{@link PaginatedResult} object
-  /// containing an array of [Stats]{@link Stats} objects. See the Stats docs.
+  /// statistics. You must specify the [start] time from which stats are
+  /// retrieved, specified as milliseconds since the Unix epoch, and the [end]
+  /// time until stats are retrieved, specified as milliseconds since the Unix
+  /// epoch.
+  /// Set the [direction], which describes the order in which stats are returned
+  /// in. Valid values are backwards which orders stats from most recent to
+  /// oldest, or forwards which orders stats from oldest to most recent. The
+  /// default is backwards.
+  /// Provide [limit], which specifies upper limit on the number of
+  /// stats returned (the default is 100, and the maximum is 1000).
+  /// Set the [unit] as either a minute, hour, day or month. Based on the unit
+  /// selected, the given start or end times are rounded down to the start of
+  /// the relevant interval depending on the unit granularity of the query.
+  /// Returns a [PaginatedResult] object containing an array of [Stats] objects.
   ///
-  /// [start] - The time from which stats are retrieved, specified as
-  /// milliseconds since the Unix epoch.
-  /// [end] - The time until stats are retrieved, specified as milliseconds
-  /// since the Unix epoch.
-  /// [direction] - The order for which stats are returned in. Valid values are
-  /// backwards which orders stats from most recent to oldest, or forwards
-  /// which orders stats from oldest to most recent. The default is backwards.
-  /// [limit] - An upper limit on the number of stats returned. The default
-  /// is 100, and the maximum is 1000.
-  /// [unit] - minute, hour, day or month. Based on the unit selected, the given
-  /// start or end times are rounded down to the start of the relevant interval
-  /// depending on the unit granularity of the query.
-  ///
-  /// [PaginatedResult] - A [PaginatedResult]{@link PaginatedResult} object
-  /// containing an array of [Stats]{@link Stats} objects.
-  /// END CANONICAL DOCSTRING
+  /// See the [Stats docs](https://ably.com/docs/general/statistics).
+  /// END EDITED CANONICAL DOCSTRING
   // Future<PaginatedResult<Stats>> stats([Map<String, dynamic>? params]) {
   //   throw UnimplementedError();
   // }
@@ -151,16 +140,13 @@ class Rest extends PlatformObject {
   /// https://docs.ably.com/client-lib-development-guide/features/#RSC16
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// Retrieves the time from the Ably service as milliseconds since the Unix
   /// epoch. Clients that do not have access to a sufficiently well maintained
-  /// time source and wish to issue Ably [TokenRequests]{@link TokenRequest}
-  /// with a more accurate timestamp should use the
-  /// [queryTime]{@link ClientOptions#queryTime} property instead of this
-  /// method.
-  ///
-  /// [Time] - The time as milliseconds since the Unix epoch.
-  /// END CANONICAL DOCSTRING
+  /// time source and wish to issue Ably [TokenRequest]s with a more accurate
+  /// timestamp should use the [AuthOptions.queryTime] property on a
+  /// [ClientOptions] object instead of this method.
+  /// END EDITED CANONICAL DOCSTRING
   Future<DateTime> time() async {
     final time = await invokeRequest<int>(PlatformMethod.restTime);
     return DateTime.fromMillisecondsSinceEpoch(time);
@@ -171,9 +157,9 @@ class Rest extends PlatformObject {
   /// subscribing for push notifications by clientId.
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
-  /// A [Push]{@link Push} object.
-  /// END CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
+  /// A [Push] object.
+  /// END EDITED CANONICAL DOCSTRING
   late Push push;
 
   /// BEGIN LEGACY DOCSTRING
@@ -182,9 +168,9 @@ class Rest extends PlatformObject {
   /// https://docs.ably.com/client-lib-development-guide/features/#RSN1
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
-  /// A [Channels]{@link Channels} object.
-  /// END CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
+  /// A [Channels] object.
+  /// END EDITED CANONICAL DOCSTRING
   late RestChannels channels;
 
   /// BEGIN LEGACY DOCSTRING
@@ -194,10 +180,10 @@ class Rest extends PlatformObject {
   /// https://docs.ably.com/client-lib-development-guide/features/#RSH8
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
-  /// Retrieves a [LocalDevice]{@link LocalDevice} object that represents the
+  /// BEGIN EDITED CANONICAL DOCSTRING
+  /// Retrieves a [LocalDevice] object that represents the
   /// current state of the device as a target for push notifications.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   Future<LocalDevice> device() async =>
       invokeRequest<LocalDevice>(PlatformMethod.pushDevice);
 }
