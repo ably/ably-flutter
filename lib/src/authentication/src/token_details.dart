05684d018d8f1b5df8aba87c9e15f0f659e646a9
Jakub Jankowski <j.jankowski@nomtek.com>
2022-10-02 20:35:01 +0200

    Edit canonical docstrings for class TokenDetails

--- lib/src/authentication/src/token_details.dart
+++ lib/src/authentication/src/token_details.dart
@@ -6,20 +6,20 @@ import 'package:meta/meta.dart';
 /// https://docs.ably.com/client-lib-development-guide/features/#TD1
 /// END LEGACY DOCSTRING
 
-/// BEGIN CANONICAL DOCSTRING
+/// BEGIN EDITED CANONICAL DOCSTRING
 /// Contains an Ably Token and its associated metadata.
-/// END CANONICAL DOCSTRING
+/// END EDITED CANONICAL DOCSTRING
 @immutable
 class TokenDetails {
   /// BEGIN LEGACY DOCSTRING
   /// https://docs.ably.com/client-lib-development-guide/features/#TD2
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// The [Ably Token](https://ably.com/docs/core-features/authentication#ably-tokens)
   /// itself. A typical Ably Token string appears with the form
-  /// xVLyHw.A-pwh7wicf3afTfgiw4k2Ku33kcnSA7z6y8FjuYpe3QaNRTEo4.
-  /// END CANONICAL DOCSTRING
+  /// `xVLyHw.A-pwh7wicf3afTfgiw4k2Ku33kcnSA7z6y8FjuYpe3QaNRTEo4.`
+  /// END EDITED CANONICAL DOCSTRING
   final String? token;
 
   /// BEGIN LEGACY DOCSTRING
@@ -28,10 +28,10 @@ class TokenDetails {
   /// https://docs.ably.com/client-lib-development-guide/features/#TD3
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// The timestamp at which this token expires as milliseconds since the Unix
   /// epoch.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   final int? expires;
 
   /// BEGIN LEGACY DOCSTRING
@@ -40,10 +40,10 @@ class TokenDetails {
   /// https://docs.ably.com/client-lib-development-guide/features/#TD4
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// The timestamp at which this token was issued as milliseconds since the
   /// Unix epoch.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   final int? issued;
 
   /// BEGIN LEGACY DOCSTRING
@@ -52,12 +52,11 @@ class TokenDetails {
   /// https://docs.ably.com/client-lib-development-guide/features/#TD5
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// The capabilities associated with this Ably Token. The capabilities value
   /// is a JSON-encoded representation of the resource paths and associated
-  /// operations. Read more about capabilities in the
-  /// [capabilities docs](https://ably.com/docs/core-features/authentication/#capabilities-explained).
-  /// END CANONICAL DOCSTRING
+  /// operations. Read more about capabilities in the [capabilities docs](https://ably.com/docs/core-features/authentication/#capabilities-explained).
+  /// END EDITED CANONICAL DOCSTRING
   final String? capability;
 
   /// BEGIN LEGACY DOCSTRING
@@ -72,13 +71,12 @@ class TokenDetails {
   /// https://docs.ably.com/client-lib-development-guide/features/#TD6
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// The client ID, if any, bound to this Ably Token. If a client ID is
   /// included, then the Ably Token authenticates its bearer as that client ID,
   /// and the Ably Token may only be used to perform operations on behalf of
-  /// that client ID. The client is then considered to be an
-  /// [identified client](https://ably.com/docs/core-features/authentication#identified-clients).
-  /// END CANONICAL DOCSTRING
+  /// that client ID. The client is then considered to be an [identified client](https://ably.com/docs/core-features/authentication#identified-clients).
+  /// END EDITED CANONICAL DOCSTRING
   final String? clientId;
 
   /// BEGIN LEGACY DOCSTRING
@@ -98,23 +96,20 @@ class TokenDetails {
   /// https://docs.ably.com/client-lib-development-guide/features/#TD7
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
-  /// TD7	A static factory method to create a TokenDetails object from a
-  /// deserialized TokenDetails-like object or a JSON stringified TokenDetails
-  /// object. This method is provided to minimize bugs as a result of differing
-  /// types by platform for fields such as timestamp or ttl. For example, in
-  /// Ruby ttl in the TokenDetails object is exposed in seconds as that is
-  /// idiomatic for the language, yet when serialized to JSON using to_json it
+  /// BEGIN EDITED CANONICAL DOCSTRING
+  /// TD7	A static factory method to create a [TokenDetails] authentication
+  /// token object from a deserialized `TokenDetails`-like object or a
+  /// JSON stringified `TokenDetails` object.
+  ///
+  /// This method is provided to minimize bugs as a result of differing
+  /// types by platform for fields such as `timestamp` or `ttl`. For example, in
+  /// Ruby `ttl` in the `TokenDetails` object is exposed in seconds as that is
+  /// idiomatic for the language, yet when serialized to JSON using `to_json` it
   /// is automatically converted to the Ably standard which is milliseconds. By
-  /// using the fromJson() method when constructing a TokenDetails object, Ably
-  /// ensures that all fields are consistently serialized and deserialized
+  /// using the `fromMap()` method when constructing a `TokenDetails` object,
+  /// Ably ensures that all fields are consistently serialized and deserialized
   /// across platforms.
-  ///
-  /// [String | JsonObject] - A deserialized TokenDetails-like object or a JSON
-  /// stringified TokenDetails object.
-  ///
-  /// [TokenDetails] - An Ably authentication token.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   TokenDetails.fromMap(Map<String, dynamic> map)
       : capability = map['capability'] as String?,
         clientId = map['clientId'] as String?,
