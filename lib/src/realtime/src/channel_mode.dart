95dd84504b9c691b7eba9e8938abcec990b9db90
Jakub Jankowski <j.jankowski@nomtek.com>
2022-10-03 15:09:11 +0200

    Edit canonical docstrings for enum ChannelMode

--- lib/src/realtime/src/channel_mode.dart
+++ lib/src/realtime/src/channel_mode.dart
@@ -1,3 +1,6 @@
+import 'package:ably_flutter/src/realtime/realtime.dart';
+import 'package:ably_flutter/src/rest/rest.dart';
+
 /// BEGIN LEGACY DOCSTRING
 /// Set of flags that represent the capabilities of a channel for current client
 ///
@@ -6,44 +9,44 @@
 /// https://docs.ably.com/client-lib-development-guide/features/#RTL4m
 /// END LEGACY DOCSTRING
 
-/// BEGIN CANONICAL DOCSTRING
+/// BEGIN EDITED CANONICAL DOCSTRING
 /// Describes the possible flags used to configure client capabilities, using
-/// [ChannelOptions]{@link ChannelOptions}.
-/// END CANONICAL DOCSTRING
+/// [RestChannelOptions] or [RealtimeChannelOptions].
+/// END EDITED CANONICAL DOCSTRING
 enum ChannelMode {
   /// BEGIN LEGACY DOCSTRING
   /// specifies that channel can check for presence
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// The client can enter the presence set.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   presence,
 
   /// BEGIN LEGACY DOCSTRING
   /// specifies that channel can publish
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// The client can publish messages.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   publish,
 
   /// BEGIN LEGACY DOCSTRING
   /// specifies that channel can subscribe to messages
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// The client can subscribe to messages.
-  /// END CANONICAL DOCSTRING
+  /// END EIDTED CANONICAL DOCSTRING
   subscribe,
 
   /// BEGIN LEGACY DOCSTRING
   /// specifies that channel can subscribe to presence events
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// The client can receive presence messages.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   presenceSubscribe,
 }
