40623444c3bdeee8fe19d94414de724f47a3000c
Jakub Jankowski <j.jankowski@nomtek.com>
2022-10-03 22:41:17 +0200

    Edit canonical docstrings for class RestPresence

--- lib/src/platform/src/rest/rest_presence.dart
+++ lib/src/platform/src/rest/rest_presence.dart
@@ -8,10 +8,10 @@ import 'package:ably_flutter/src/platform/platform_internal.dart';
 /// https://docs.ably.com/client-lib-development-guide/features/#RSP1
 /// END LEGACY DOCSTRING
 
-/// BEGIN CANONICAL DOCSTRING
+/// BEGIN EDITED CANONICAL DOCSTRING
 /// Enables the retrieval of the current and historic presence set for a
 /// channel.
-/// END CANONICAL DOCSTRING
+/// END EDITED CANONICAL DOCSTRING
 class RestPresence extends PlatformObject {
   final RestChannel _restChannel;
 
@@ -29,23 +29,14 @@ class RestPresence extends PlatformObject {
   /// https://docs.ably.com/client-lib-development-guide/features/#RSP3
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// Retrieves the current members present on the channel and the metadata for
-  /// each member, such as their [PresenceAction]{@link PresenceAction} and ID.
-  /// Returns a [PaginatedResult]{@link PaginatedResult} object, containing an
-  /// array of [PresenceMessage]{@link PresenceMessage} objects.
+  /// each member, such as their [PresenceAction] and ID, based on provided
+  /// [params].
   ///
-  /// [limit] - An upper limit on the number of messages returned. The default
-  /// is 100, and the maximum is 1000.
-  /// [clientId] - Filters the list of returned presence members by a specific
-  /// client using its ID.
-  /// [connectionId] - Filters the list of returned presence members by a
-  /// specific connection using its ID.
-  ///
-  /// [PaginatedResult<PresenceMessage>] - 	A
-  /// [PaginatedResult]{@link PaginatedResult} object containing an array of
-  /// [PresenceMessage]{@link PresenceMessage} objects.
-  /// END CANONICAL DOCSTRING
+  /// Returns a [PaginatedResult] object, containing an
+  /// array of [PresenceMessage] objects.
+  /// END EDITED CANONICAL DOCSTRING
   Future<PaginatedResult<PresenceMessage>> get([
     RestPresenceParams? params,
   ]) async {
@@ -67,29 +58,16 @@ class RestPresence extends PlatformObject {
   /// https://docs.ably.com/client-lib-development-guide/features/#RSP4
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
-  /// Retrieves a [PaginatedResult]{@link PaginatedResult} object, containing an
-  /// array of historical [PresenceMessage]{@link PresenceMessage} objects for
-  /// the channel. If the channel is configured to persist messages, then
+  /// BEGIN EDITED CANONICAL DOCSTRING
+  /// Retrieves a [PaginatedResult] object, containing an
+  /// array of historical [PresenceMessage] objects for
+  /// the channel, based on provided [params].
+  ///
+  /// If the channel is configured to persist messages, then
   /// presence messages can be retrieved from history for up to 72 hours in the
   /// past. If not, presence messages can only be retrieved from history for up
   /// to two minutes in the past.
-  ///
-  /// [start] - The time from which messages are retrieved, specified as
-  /// milliseconds since the Unix epoch.
-  /// [end] - The time until messages are retrieved, specified as milliseconds
-  /// since the Unix epoch.
-  /// [direction] - The order for which messages are returned in. Valid values
-  /// are backwards which orders messages from most recent to oldest, or
-  /// forwards which orders messages from oldest to most recent. The default is
-  /// backwards.
-  /// [limit] - An upper limit on the number of messages returned. The default
-  /// is 100, and the maximum is 1000.
-  ///
-  /// [PaginatedResult<PresenceMessage>] - 	A
-  /// [PaginatedResult]{@link PaginatedResult} object containing an array of
-  /// [PresenceMessage]{@link PresenceMessage} objects.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   Future<PaginatedResult<PresenceMessage>> history([
     RestHistoryParams? params,
   ]) async {
