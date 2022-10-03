d7a1b05d8dd8707417dd60f1f5c3230458b40cb3
Jakub Jankowski <j.jankowski@nomtek.com>
2022-10-03 13:05:19 +0200

    Edit canonical docstrings for class RealtimeHistoryParams

--- lib/src/realtime/src/realtime_history_params.dart
+++ lib/src/realtime/src/realtime_history_params.dart
@@ -16,10 +16,9 @@ class RealtimeHistoryParams {
   /// https://docs.ably.com/client-lib-development-guide/features/#RTL10a
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRINGS
-  /// The time from which messages are retrieved, specified as
-  /// milliseconds since the Unix epoch.
-  /// END CANONICAL DOCSTRINGS
+  /// BEGIN EDITED CANONICAL DOCSTRINGS
+  /// The [DateTime] from which messages are retrieved.
+  /// END EDITED CANONICAL DOCSTRINGS
   final DateTime start;
 
   /// BEGIN LEGACY DOCSTRING
@@ -31,10 +30,9 @@ class RealtimeHistoryParams {
   /// https://docs.ably.com/client-lib-development-guide/features/#RTL10a
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRINGS
-  /// The time until messages are retrieved, specified as milliseconds
-  /// since the Unix epoch.
-  /// END CANONICAL DOCSTRINGS
+  /// BEGIN EDITED CANONICAL DOCSTRINGS
+  /// The [DateTime] until messages are retrieved.
+  /// END EDITED CANONICAL DOCSTRINGS
   final DateTime end;
 
   /// BEGIN LEGACY DOCSTRING
@@ -45,12 +43,12 @@ class RealtimeHistoryParams {
   /// https://docs.ably.com/client-lib-development-guide/features/#RTL10a
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRINGS
+  /// BEGIN EDITED CANONICAL DOCSTRINGS
   /// The order for which messages are returned in. Valid values
-  /// are backwards which orders messages from most recent to oldest, or
-  /// forwards which orders messages from oldest to most recent. The default is
-  /// backwards.
-  /// END CANONICAL DOCSTRINGS
+  /// are `backwards` which orders messages from most recent to oldest, or
+  /// `forwards` which orders messages from oldest to most recent. The default
+  /// is `backwards`.
+  /// END EDITED CANONICAL DOCSTRINGS
   final String direction;
 
   /// BEGIN LEGACY DOCSTRING
@@ -62,10 +60,10 @@ class RealtimeHistoryParams {
   /// https://docs.ably.com/client-lib-development-guide/features/#RTL10a
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRINGS
-  /// An upper limit on the number of messages returned. The default
-  /// is 100, and the maximum is 1000.
-  /// END CANONICAL DOCSTRINGS
+  /// BEGIN EDITED CANONICAL DOCSTRINGS
+  /// An upper limit on the number of messages returned. The default is 100, and
+  /// the maximum is 1000.
+  /// END EDITED CANONICAL DOCSTRINGS
   final int limit;
 
   /// BEGIN LEGACY DOCSTRING
@@ -77,10 +75,13 @@ class RealtimeHistoryParams {
   /// https://docs.ably.com/client-lib-development-guide/features/#RTL10b
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRINGS
-  /// When true, ensures message history is up until the point
-  /// of the channel being attached. See
-  /// END CANONICAL DOCSTRINGS
+  /// BEGIN EDITED CANONICAL DOCSTRINGS
+  /// Whether it's ensured that the message history is up until the point
+  /// of the channel being attached. See [continuous history](https://ably.com/docs/realtime/history#continuous-history)
+  /// for more info. Requires the direction to be `backwards`. If the channel is
+  /// not attached, or if `direction` is set to `forwards`, this option results
+  /// in an error.
+  /// END EDITED CANONICAL DOCSTRINGS
   final bool? untilAttach;
 
   /// BEGIN LEGACY DOCSTRING
