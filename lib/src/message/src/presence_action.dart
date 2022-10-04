080af7a8af089e993b8661a314b8a5a711602dba
Jakub Jankowski <j.jankowski@nomtek.com>
2022-10-04 13:10:17 +0200

    Edit canonical docstrings for enum PresenceAction

--- lib/src/message/src/presence_action.dart
+++ lib/src/message/src/presence_action.dart
@@ -6,28 +6,28 @@ import 'package:ably_flutter/ably_flutter.dart';
 /// https://docs.ably.com/client-lib-development-guide/features/#TP2
 /// END LEGACY DOCSTRING
 
-/// BEGIN CANONICAL DOCSTRING
+/// BEGIN EDITED CANONICAL DOCSTRING
 /// Describes the possible actions members in the presence set can emit.
-/// END CANONICAL DOCSTRING
+/// END EDITED CANONICAL DOCSTRING
 enum PresenceAction {
   /// BEGIN LEGACY DOCSTRING
   /// indicates that a client is absent for incoming [PresenceMessage]
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// A member is not present in the channel.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   absent,
 
   /// BEGIN LEGACY DOCSTRING
   /// indicates that a client is present for incoming [PresenceMessage]
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// When subscribing to presence events on a channel that already has members
   /// present, this event is emitted for every member already present on the
   /// channel before the subscribe listener was registered.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   present,
 
   /// BEGIN LEGACY DOCSTRING
@@ -35,9 +35,9 @@ enum PresenceAction {
   /// outgoing [PresenceMessage]
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// A new member has entered the channel.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   enter,
 
   /// BEGIN LEGACY DOCSTRING
@@ -45,13 +45,13 @@ enum PresenceAction {
   /// outgoing [PresenceMessage]
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// A member who was present has now left the channel. This may be a result of
   /// an explicit request to leave or implicitly when detaching from the
   /// channel. Alternatively, if a member's connection is abruptly disconnected
   /// and they do not resume their connection within a minute, Ably treats this
   /// as a leave event as the client is no longer present.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   leave,
 
   /// BEGIN LEGACY DOCSTRING
@@ -59,10 +59,10 @@ enum PresenceAction {
   /// needs to be updated
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// An already present member has updated their member data. Being notified of
   /// member data updates can be very useful, for example, it can be used to
   /// update the status of a user when they are typing a message.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   update,
 }
