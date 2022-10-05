d4a76ff5c04cf86327fae28ce07a6563bcd81243
Jakub Jankowski <j.jankowski@nomtek.com>
2022-10-05 10:39:50 +0200

    Edit cannonical docstrings for class Connection.

--- lib/src/platform/src/realtime/connection.dart
+++ lib/src/platform/src/realtime/connection.dart
@@ -9,9 +9,9 @@ import 'package:ably_flutter/src/platform/platform_internal.dart';
 /// https://docs.ably.com/client-lib-development-guide/features/#RTN1
 /// END LEGACY DOCSTRING
 
-/// BEGIN CANONICAL DOCSTRING
+/// BEGIN EDITED CANONICAL DOCSTRING
 /// Enables the management of a connection to Ably.
-/// END CANONICAL DOCSTRING
+/// END EDITED CANONICAL DOCSTRING
 class Connection extends PlatformObject {
   /// Realtime client instance
   final Realtime realtime;
@@ -46,10 +46,10 @@ class Connection extends PlatformObject {
   /// https://docs.ably.com/client-lib-development-guide/features/#RTN15
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
-  /// An [ErrorInfo]{@link ErrorInfo} object describing the last error received
-  /// if a connection failure occurs.
-  /// END CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
+  /// An [ErrorInfo] object describing the last error received if a connection
+  /// failure occurs.
+  /// END EDITED CANONICAL DOCSTRING
   ErrorInfo? get errorReason => _errorReason;
 
   /// BEGIN LEGACY DOCSTRING
@@ -59,10 +59,10 @@ class Connection extends PlatformObject {
   /// https://docs.ably.com/client-lib-development-guide/features/#RTN8
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// A unique public identifier for this connection, used to identify this
   /// member.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   String? id;
 
   /// BEGIN LEGACY DOCSTRING
@@ -72,15 +72,15 @@ class Connection extends PlatformObject {
   /// https://docs.ably.com/client-lib-development-guide/features/#RTN9
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// A unique private connection key used to recover or resume a connection,
-  /// assigned by Ably. When recovering a connection explicitly, the recoveryKey
-  /// is used in the recover client options as it contains both the key and the
-  /// last message serial. This private connection key can also be used by other
-  /// REST clients to publish on behalf of this client. See the
+  /// assigned by Ably. When recovering a connection explicitly, the
+  /// `recoveryKey` is used in the recover client options as it contains both
+  /// the key and the last message serial. This private connection key can also
+  /// be used by other REST clients to publish on behalf of this client. See the
   /// [publishing over REST on behalf of a realtime client docs](https://ably.com/docs/rest/channels#publish-on-behalf)
   /// for more info.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   String? key;
 
   /// BEGIN LEGACY DOCSTRING
@@ -88,12 +88,12 @@ class Connection extends PlatformObject {
   /// connection key and latest serial received on the connection
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// The recovery key string can be used by another client to recover this
   /// connection's state in the recover client options property. See
   /// [connection state recover options](https://ably.com/docs/realtime/connection#connection-state-recover-options)
   /// for more information.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   String? recoveryKey;
 
   /// BEGIN LEGACY DOCSTRING
@@ -102,13 +102,13 @@ class Connection extends PlatformObject {
   /// https://docs.ably.com/client-lib-development-guide/features/#RTN10
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// The serial number of the last message to be received on this connection,
   /// used automatically by the library when recovering or resuming a
-  /// connection. When recovering a connection explicitly, the recoveryKey is
+  /// connection. When recovering a connection explicitly, the `recoveryKey` is
   /// used in the recover client options as it contains both the key and the
   /// last message serial.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   int? serial;
 
   /// BEGIN LEGACY DOCSTRING
@@ -117,9 +117,9 @@ class Connection extends PlatformObject {
   /// https://docs.ably.com/client-lib-development-guide/features/#connection-states-operations
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
-  /// The current [ConnectionState]{@link ConnectionState} of the connection.
-  /// END CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
+  /// The current [ConnectionState] of the connection.
+  /// END EDITED CANONICAL DOCSTRING
   ConnectionState get state => _state;
 
   /// BEGIN LEGACY DOCSTRING
@@ -138,12 +138,12 @@ class Connection extends PlatformObject {
   /// https://docs.ably.com/client-lib-development-guide/features/#RTN12
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// Causes the connection to close, entering the
-  /// [CLOSING]{@link ConnectionState#CLOSING} state. Once closed, the library
-  /// does not attempt to re-establish the connection without an explicit call
-  /// to [connect()]{@link Connection#connect}.
-  /// END CANONICAL DOCSTRING
+  /// [ConnectionState.closing] state. Once closed, the library does not attempt
+  /// to re-establish the connection without an explicit call to
+  /// [Connection.connect]
+  /// END EDITED CANONICAL DOCSTRING
   Future<void> close() => realtime.close();
 
   /// BEGIN LEGACY DOCSTRING
@@ -152,12 +152,12 @@ class Connection extends PlatformObject {
   /// https://docs.ably.com/client-lib-development-guide/features/#RTN11
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
-  /// Explicitly calling connect() is unnecessary unless the autoConnect
-  /// attribute of the [ClientOptions]{@link ClientOptions} object is false.
+  /// BEGIN EDITED CANONICAL DOCSTRING
+  /// Explicitly calling `connect()` is unnecessary unless the `autoConnect`
+  /// attribute of the [ClientOptions] object is false.
   /// Unless already connected or connecting, this method causes the connection
-  /// to open, entering the [CONNECTING]{@link ConnectionState#CONNECTING} state.
-  /// END CANONICAL DOCSTRING
+  /// to open, entering the [ConnectionState.connecting] state.
+  /// END EDITED CANONICAL DOCSTRING
   Future<void> connect() => realtime.connect();
 
   /// BEGIN LEGACY DOCSTRING
@@ -168,14 +168,13 @@ class Connection extends PlatformObject {
   /// https://docs.ably.com/client-lib-development-guide/features/#RTN13
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// When connected, sends a heartbeat ping to the Ably server and executes the
   /// callback with any error and the response time in milliseconds when a
   /// heartbeat ping request is echoed from the server. This can be useful for
   /// measuring true round-trip latency to the connected Ably server.
-  ///
-  /// [Duration] - The response time in milliseconds.
-  /// END CANONICAL DOCSTRING
+  /// Returns the response time in miliseconds as [int]
+  /// END EDITED CANONICAL DOCSTRING
   Future<int> ping() {
     throw UnimplementedError();
   }
