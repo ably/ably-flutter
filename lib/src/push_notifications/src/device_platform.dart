78c83664d941366c3e7c35af2139ee328f3af28e
Jakub Jankowski <j.jankowski@nomtek.com>
2022-10-05 14:48:28 +0200

    Edit canonical docstrings for class DevicePlatform

--- lib/src/push_notifications/src/device_platform.dart
+++ lib/src/push_notifications/src/device_platform.dart
@@ -5,34 +5,34 @@ import 'package:ably_flutter/ably_flutter.dart';
 /// in [DeviceDetails] while registering
 /// END LEGACY DOCSTRING
 
-/// BEGIN CANONICAL DOCSTRING
+/// BEGIN EDITED CANONICAL DOCSTRING
 /// Describes the device receiving push notifications.
-/// END CANONICAL DOCSTRING
+/// END EDITED CANONICAL DOCSTRING
 enum DevicePlatform {
   /// BEGIN LEGACY DOCSTRING
   /// indicates an android device
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// The device platform is Android.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   android,
 
   /// BEGIN LEGACY DOCSTRING
   /// indicates an iOS device
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// The device platform is iOS.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   ios,
 
   /// BEGIN LEGACY DOCSTRING
   /// indicates a browser
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// The device platform is a web browser.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   browser,
 }
