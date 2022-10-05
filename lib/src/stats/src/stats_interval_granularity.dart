da89caf346e0f9873c425042f16886082c9c9ed0
Jakub Jankowski <j.jankowski@nomtek.com>
2022-10-05 11:58:31 +0200

    Edit canonical docstrings for enum StatsIntervalGranularity

--- lib/src/stats/src/stats_interval_granularity.dart
+++ lib/src/stats/src/stats_interval_granularity.dart
@@ -2,43 +2,43 @@
 /// https://docs.ably.com/client-lib-development-guide/features/#TS12c
 /// END LEGACY DOCSTRING
 
-/// BEGIN CANONICAL DOCSTRING
+/// BEGIN EDITED  CANONICAL DOCSTRING
 /// Describes the interval unit over which statistics are gathered.
-/// END CANONICAL DOCSTRING
+/// END EDITED CANONICAL DOCSTRING
 enum StatsIntervalGranularity {
   /// BEGIN LEGACY DOCSTRING
   /// indicates units in minutes
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// Interval unit over which statistics are gathered as minutes.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   minute,
 
   /// BEGIN LEGACY DOCSTRING
   /// indicates units in hours
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// Interval unit over which statistics are gathered as hours.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   hour,
 
   /// BEGIN LEGACY DOCSTRING
   /// indicates units in days
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// Interval unit over which statistics are gathered as days.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   day,
 
   /// BEGIN LEGACY DOCSTRING
   /// indicates units in months
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// Interval unit over which statistics are gathered as months.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   month,
 }
