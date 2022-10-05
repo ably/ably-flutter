6c6e822ce146fa9deaa5e219735b9ca9bc44b2bc
Jakub Jankowski <j.jankowski@nomtek.com>
2022-10-05 14:47:41 +0200

    Edit canonical docstrings for class PushChannelSubscriptions

--- lib/src/push_notifications/src/push_channel_subscriptions.dart
+++ lib/src/push_notifications/src/push_channel_subscriptions.dart
@@ -4,9 +4,9 @@ import 'package:ably_flutter/ably_flutter.dart';
 /// Manage push notification channel subscriptions for devices or clients
 /// END LEGACY DOCSTRING
 
-/// BEGIN CANONICAL DOCSTRING
+/// BEGIN EDITED CANONICAL DOCSTRING
 /// Enables device push channel subscriptions.
-/// END CANONICAL DOCSTRING
+/// END EDITED CANONICAL DOCSTRING
 abstract class PushChannelSubscriptions {
   /// BEGIN LEGACY DOCSTRING
   /// List channel subscriptions filtered by optional params.
@@ -14,20 +14,11 @@ abstract class PushChannelSubscriptions {
   /// https://docs.ably.com/client-lib-development-guide/features/#RSH1c1
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
-  /// Retrieves all push channel subscriptions matching the filter params
-  /// provided. Returns a [PaginatedResult]{@link PaginatedResult} object,
-  /// containing an array of
-  /// [PushChannelSubscription]{@link PushChannelSubscription} objects.
-  /// END CANONICAL DOCSTRING
-  ///
-  /// [params] - An object containing key-value pairs to filter subscriptions
-  /// by. Can contain channel, clientId, deviceId and a limit on the number of
-  /// devices returned, up to 1,000.
-  ///
-  /// [PaginatedResult<PushChannelSubscription>] - A
-  /// PaginatedResult]{@link PaginatedResult} object containing an array of
-  /// [PushChannelSubscription]{@link PushChannelSubscription} objects.
+  /// BEGIN EDITED CANONICAL DOCSTRING
+  /// Retrieves all push channel subscriptions matching the filter [params]
+  /// provided. Returns a [PaginatedResult] object, containing an array of
+  /// [PushChannelSubscription] objects.
+  /// END EDITED CANONICAL DOCSTRING
   Future<PaginatedResult<PushChannelSubscription>> list(
     PushChannelSubscriptionParams params,
   );
@@ -38,16 +29,11 @@ abstract class PushChannelSubscriptions {
   /// https://docs.ably.com/client-lib-development-guide/features/#RSH1c2
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// Retrieves all channels with at least one device subscribed to push
-  /// notifications. Returns a [PaginatedResult]{@link PaginatedResult} object,
+  /// notifications using a [params] object. Returns a [PaginatedResult] object,
   /// containing an array of channel names.
-  ///
-  /// [params] - An object containing key-value pairs to filter channels by. Can
-  /// contain a limit on the number of channels returned, up to 1,000.
-  /// [PaginatedResult<String] - A [PaginatedResult]{@link PaginatedResult}
-  /// object containing an array of channel names.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   Future<PaginatedResult<String>> listChannels(
     PushChannelsParams params,
   );
@@ -58,17 +44,12 @@ abstract class PushChannelSubscriptions {
   /// https://docs.ably.com/client-lib-development-guide/features/#RSH1c3
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
-  /// Subscribes a device, or a group of devices sharing the same clientId to
-  /// push notifications on a channel. Returns a
-  /// [PushChannelSubscription]{@link PushChannelSubscription} object.
-  ///
-  /// [PushChannelSubscription] - A [PushChannelSubscription]{@link PushChannelSubscription}
-  /// object.
-  ///
-  /// [PushChannelSubscription] - 	A [PushChannelSubscription]{@link PushChannelSubscription}
-  /// object describing the new or updated subscriptions.
-  /// END CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
+  /// Subscribes a device, or a group of devices sharing the same `clientId` to
+  /// push notifications on a channel, using the [subscription] object. Returns
+  /// a [PushChannelSubscription] object describing the new or updated
+  /// subscriptions.
+  /// END EDITED CANONICAL DOCSTRING
   Future<PushChannelSubscription> save(PushChannelSubscription subscription);
 
   /// BEGIN LEGACY DOCSTRING
@@ -77,13 +58,11 @@ abstract class PushChannelSubscriptions {
   /// https://docs.ably.com/client-lib-development-guide/features/#RSH1c4
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
-  /// Unsubscribes a device, or a group of devices sharing the same clientId
-  /// from receiving push notifications on a channel.
-  ///
-  /// [PushChannelSubscription] - A
-  /// [PushChannelSubscription]{@link PushChannelSubscription} object.
-  /// END CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
+  /// Unsubscribes a device, or a group of devices sharing the same `clientId`
+  /// from receiving push notifications on a channel, using the [subscription]
+  /// object.
+  /// END EDITED CANONICAL DOCSTRING
   Future<void> remove(PushChannelSubscription subscription);
 
   /// BEGIN LEGACY DOCSTRING
@@ -92,12 +71,9 @@ abstract class PushChannelSubscriptions {
   /// https://docs.ably.com/client-lib-development-guide/features/#RSH1c5
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// Unsubscribes all devices from receiving push notifications on a channel
-  /// that match the filter params provided.
-  ///
-  /// [params] - An object containing key-value pairs to filter subscriptions
-  /// by. Can contain channel, and optionally either clientId or deviceId.
-  /// END CANONICAL DOCSTRING
+  /// that match the filter [params] object provided.
+  /// END EDITED CANONICAL DOCSTRING
   Future<void> removeWhere(PushChannelSubscriptionParams params);
 }
