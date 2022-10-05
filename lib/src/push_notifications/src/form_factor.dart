73bc31755bd4b07ae5482c808b22c091fd89d691
Jakub Jankowski <j.jankowski@nomtek.com>
2022-10-05 14:49:55 +0200

    Edit canonical docstrings for enum DeviceFormFactor

--- lib/src/push_notifications/src/form_factor.dart
+++ lib/src/push_notifications/src/form_factor.dart
@@ -6,79 +6,79 @@ import 'package:ably_flutter/ably_flutter.dart';
 /// https://docs.ably.com/client-lib-development-guide/features/#PCD4
 /// END LEGACY DOCSTRING
 
-/// BEGIN CANONICAL DOCSTRING
+/// BEGIN EDITED CANONICAL DOCSTRING
 /// Describes the type of device receiving a push notification.
-/// END CANONICAL DOCSTRING
+/// END EDITED CANONICAL DOCSTRING
 enum FormFactor {
   /// BEGIN LEGACY DOCSTRING
   /// indicates the device is a mobile phone
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// The device is a phone.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   phone,
 
   /// BEGIN LEGACY DOCSTRING
   /// indicates the device is a tablet
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// The device is tablet.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   tablet,
 
   /// BEGIN LEGACY DOCSTRING
   /// indicates the device is a desktop
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// The device is a desktop.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   desktop,
 
   /// BEGIN LEGACY DOCSTRING
   /// indicates the device is a television
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGI EDITED CANONICAL DOCSTRING
   /// The device is a TV.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   tv,
 
   /// BEGIN LEGACY DOCSTRING
   /// indicates the device is a smart watch
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// The device is a watch.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   watch,
 
   /// BEGIN LEGACY DOCSTRING
   /// indicates the device is an automobile
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// The device is a car.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   car,
 
   /// BEGIN LEGACY DOCSTRING
   /// indicates the device is an embedded system / iOT device
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// The device is embedded.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   embedded,
 
   /// BEGIN LEGACY DOCSTRING
   /// indicates the device belong to categories other than mentioned above
   /// END LEGACY DOCSTRING
 
-  /// BEGIN CANONICAL DOCSTRING
+  /// BEGIN EDITED CANONICAL DOCSTRING
   /// The device is other.
-  /// END CANONICAL DOCSTRING
+  /// END EDITED CANONICAL DOCSTRING
   other,
 }
