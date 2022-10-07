68d6122f61e676079b1d90ddb9902feeaf40a291
Jakub Jankowski <j.jankowski@nomtek.com>
2022-10-07 16:21:42 +0200

    Add and edit missing docstrings for class RestChannels

--- lib/src/platform/src/rest/rest_channels.dart
+++ lib/src/platform/src/rest/rest_channels.dart
@@ -7,6 +7,10 @@ import 'package:meta/meta.dart';
 ///
 /// https://docs.ably.com/client-lib-development-guide/features/#RSN1
 /// END LEGACY DOCSTRING
+
+/// BEGIN EDITED CANONICAL DOCSTRING
+/// Creates and destroys [RestChannel] objects.
+/// END EDITED CANONICAL DOCSTRING
 class RestChannels extends Channels<RestChannel> {
   final Rest _rest;
 
@@ -15,11 +19,22 @@ class RestChannels extends Channels<RestChannel> {
   /// END LEGACY DOCSTRING
   RestChannels(this._rest);
 
+  /// BEGIN LEGACY DOCSTRING
+  /// creates a channel with provided name and options
+  ///
+  /// This is a private method to be overridden by implementation classes
+  /// END LEGACY DOCSTRING
   @override
   @protected
   RestChannel createChannel(String name) =>
       RestChannel(_rest, PushChannel(name, rest: _rest), name);
 
+  /// BEGIN EDITED CANONICAL DOCSTRING
+  /// Releases a [RestChannel] object with a specified [name], deleting it. It
+  /// also removes any listeners associated with the channel. To release a
+  /// channel, the [ChannelState] must be `INITIALIZED`, `DETACHED`, or
+  /// `FAILED`.
+  /// END EDITED CANONICAL DOCSTRING
   @override
   void release(String name) {
     _rest.invoke<void>(PlatformMethod.releaseRestChannel, {
