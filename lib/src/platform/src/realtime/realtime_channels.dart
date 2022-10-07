674d134caa197571ea31be6cfb23a08e244c8a18
Jakub Jankowski <j.jankowski@nomtek.com>
2022-10-07 16:21:27 +0200

    Add and edit missing docstrings for class RealtimeChannels

--- lib/src/platform/src/realtime/realtime_channels.dart
+++ lib/src/platform/src/realtime/realtime_channels.dart
@@ -7,6 +7,10 @@ import 'package:meta/meta.dart';
 ///
 /// https://docs.ably.com/client-lib-development-guide/features/#RTS1
 /// END LEGACY DOCSTRING
+
+/// BEGIN EDITED CANONICAL DOCSTRING
+/// Creates and destroys [RealtimeChannel] objects.
+/// END EDITED CANONICAL DOCSTRING
 class RealtimeChannels extends Channels<RealtimeChannel> {
   /// BEGIN LEGACY DOCSTRING
   /// instance of ably realtime client
@@ -26,6 +30,12 @@ class RealtimeChannels extends Channels<RealtimeChannel> {
   /// Detaches the channel and then releases the channel resource
   /// so it can be garbage collected.
   /// END LEGACY DOCSTRING
+
+  /// BEGIN EDITED CANONICAL DOCSTRING
+  /// Releases a [RealtimeChannel] object with a specified [name], deleting it.
+  /// It also removes any listeners associated with the channel. To release a
+  /// channel, the [ChannelState] must be `INITIALIZED`, `DETACHED`, or `FAILED`f.
+  /// END EDITED CANONICAL DOCSTRING
   @override
   void release(String name) {
     realtime.invoke<void>(PlatformMethod.releaseRealtimeChannel, {
