31db0ffb49e07731e137e80edc445b097ae70401
Jakub Jankowski <j.jankowski@nomtek.com>
2022-11-04 11:41:54 +0100

    Add and edit canonical docstrings for class RealtimePresenceParams

--- lib/src/realtime/src/realtime_presence_params.dart
+++ lib/src/realtime/src/realtime_presence_params.dart
@@ -14,6 +14,15 @@ class RealtimePresenceParams {
   ///
   /// https://docs.ably.com/client-lib-development-guide/features/#RTP11c1
   /// END LEGACY DOCSTRING
+
+  /// BEGIN CANONICAL DOCSTRING
+  /// Whether to wait for a full presence set synchronization between Ably
+  /// and the clients on the channel to complete before returning the results.
+  /// Synchronization begins as soon as the channel is [ChannelState.attached].
+  /// When set to true the results will be returned as soon as the sync is
+  /// complete. When set to false the current list of members will be returned
+  /// without the sync completing. The default is true.
+  /// END CANONICAL DOCSTRING
   final bool waitForSync;
 
   /// BEGIN LEGACY DOCSTRING
@@ -21,6 +30,11 @@ class RealtimePresenceParams {
   ///
   /// https://docs.ably.com/client-lib-development-guide/features/#RTP11c2
   /// END LEGACY DOCSTRING
+
+  /// BEGIN CANONICAL DOCSTRING
+  /// Filters the array of returned presence members by a specific client using
+  /// its ID.
+  /// END CANONICAL DOCSTRING
   final String? clientId;
 
   /// BEGIN LEGACY DOCSTRING
@@ -28,6 +42,11 @@ class RealtimePresenceParams {
   ///
   /// https://docs.ably.com/client-lib-development-guide/features/#RTP11c3
   /// END LEGACY DOCSTRING
+
+  /// BEGIN CANONICAL DOCSTRING
+  /// Filters the array of returned presence members by a specific connection
+  /// using its ID.
+  /// END CANONICAL DOCSTRING
   final String? connectionId;
 
   /// BEGIN LEGACY DOCSTRING
