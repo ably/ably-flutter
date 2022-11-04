5aecf6cf576658d9017d423cb28f3f4f11349d61
Jakub Jankowski <j.jankowski@nomtek.com>
2022-11-04 11:36:21 +0100

    Mark legacy docstrings for class RealtimePresenceParams

--- lib/src/realtime/src/realtime_presence_params.dart
+++ lib/src/realtime/src/realtime_presence_params.dart
@@ -1,28 +1,38 @@
 import 'package:ably_flutter/ably_flutter.dart';
 import 'package:meta/meta.dart';
 
+/// BEGIN LEGACY DOCSTRING
 /// Params used as a filter for querying presence on a channel
 ///
 /// https://docs.ably.com/client-lib-development-guide/features/#RTP11c
+/// END LEGACY DOCSTRING
 @immutable
 class RealtimePresenceParams {
+  /// BEGIN LEGACY DOCSTRING
   /// When true, [RealtimePresence.get] will wait until SYNC is complete
   /// before returning a list of members
   ///
   /// https://docs.ably.com/client-lib-development-guide/features/#RTP11c1
+  /// END LEGACY DOCSTRING
   final bool waitForSync;
 
+  /// BEGIN LEGACY DOCSTRING
   /// filters members by the provided clientId
   ///
   /// https://docs.ably.com/client-lib-development-guide/features/#RTP11c2
+  /// END LEGACY DOCSTRING
   final String? clientId;
 
+  /// BEGIN LEGACY DOCSTRING
   /// filters members by the provided connectionId
   ///
   /// https://docs.ably.com/client-lib-development-guide/features/#RTP11c3
+  /// END LEGACY DOCSTRING
   final String? connectionId;
 
+  /// BEGIN LEGACY DOCSTRING
   /// initializes with [waitForSync] set to true by default
+  /// END LEGACY DOCSTRING
   const RealtimePresenceParams({
     this.clientId,
     this.connectionId,
