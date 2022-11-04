1f8663ed12840fb03b74b99113ba405d87a7114c
Jakub Jankowski <j.jankowski@nomtek.com>
2022-11-04 11:14:51 +0100

    Add and edit missing canonical docstrings for class DeviceRegistrationParams.

--- lib/src/push_notifications/src/device_registration_params.dart
+++ lib/src/push_notifications/src/device_registration_params.dart
@@ -10,20 +10,36 @@ abstract class DeviceRegistrationParams {
   /// BEGIN LEGACY DOCSTRING
   /// filter by client id
   /// END LEGACY DOCSTRING
+
+  /// BEGIN CANONICAL DOCSTRING
+  /// The ID of the client.
+  /// END CANONICAL DOCSTRING
   String? clientId;
 
   /// BEGIN LEGACY DOCSTRING
   /// filter by device id
   /// END LEGACY DOCSTRING
+
+  /// BEGIN EDITED CANONICAL DOCSTRING
+  /// The unique ID of the device.
+  /// END EDITED CANONICAL DOCSTRING
   String? deviceId;
 
   /// BEGIN LEGACY DOCSTRING
   /// limit results for each page
   /// END LEGACY DOCSTRING
+
+  /// BEGIN EDITED CANONICAL DOCSTRING
+  /// Limit on the number of devices returned, up to 1,000.
+  /// END EDITED CANONICAL DOCSTRING
   int? limit;
 
   /// BEGIN LEGACY DOCSTRING
   /// filter by device state
   /// END LEGACY DOCSTRING
+
+  /// BEGIN EDITED CANONICAL DOCSTRING
+  /// The current state of the push registration.
+  /// END EDITED CANONICAL DOCSTRING
   DevicePushState? state;
 }
