f3c7ede6a5b4ac6779b3e57de220a761568df09f
Jakub Jankowski <j.jankowski@nomtek.com>
2022-11-04 11:27:07 +0100

    Add edited canonical docstrings for PushChannelSubscriptionParams class.

--- lib/src/push_notifications/src/push_channel_subscription_params.dart
+++ lib/src/push_notifications/src/push_channel_subscription_params.dart
@@ -10,20 +10,36 @@ abstract class PushChannelSubscriptionParams {
   /// BEGIN LEGACY DOCSTRING
   /// filter by channel
   /// END LEGACY DOCSTRING
+
+  /// BEGIN EDITED CANONICAL DOCSTRING
+  /// The channel name.
+  /// END EDITED CANONICAL DOCSTRING
   String? channel;
 
   /// BEGIN LEGACY DOCSTRING
   /// filter by clientId
   /// END LEGACY DOCSTRING
+
+  /// BEGIN EDITED CANONICAL DOCSTRING
+  /// The ID of the client.
+  /// END EDITED CANONICAL DOCSTRING
   String? clientId;
 
   /// BEGIN LEGACY DOCSTRING
   /// filter by deviceId
   /// END LEGACY DOCSTRING
+
+  /// BEGIN EDITED CANONICAL DOCSTRING
+  /// The unique ID of the device.
+  /// END EDITED CANONICAL DOCSTRING
   String? deviceId;
 
   /// BEGIN LEGACY DOCSTRING
   /// limit results for each page
   /// END LEGACY DOCSTRING
+
+  /// BEGIN EDITED CANONICAL DOCSTRING
+  /// A limit on the number of devices returned, up to 1,000.
+  /// END EDITED CANONICAL DOCSTRING
   int? limit;
 }
