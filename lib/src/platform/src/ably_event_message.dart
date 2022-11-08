d3ec477673973d149f75cce8e8da2e629116f8ca
Jakub Jankowski <j.jankowski@nomtek.com>
2022-11-08 16:04:01 +0100

    Handle docstrings for constructors/properties that are not documented in canonical api ref in AblyEventMessage class

--- lib/src/platform/src/ably_event_message.dart
+++ lib/src/platform/src/ably_event_message.dart
@@ -1,21 +1,25 @@
 import 'package:meta/meta.dart';
 
 /// BEGIN LEGACY DOCSTRING
+/// @nodoc
 /// An encapsulating object used to pass data to platform for registering events
 /// END LEGACY DOCSTRING
 @immutable
 class AblyEventMessage {
   /// BEGIN LEGACY DOCSTRING
+  /// @nodoc
   /// name of the event to register a listener for
   /// END LEGACY DOCSTRING
   final String eventName;
 
   /// BEGIN LEGACY DOCSTRING
+  /// @nodoc
   /// data to be passed for starting a listener
   /// END LEGACY DOCSTRING
   final Object? message;
 
   /// BEGIN LEGACY DOCSTRING
+  /// @nodoc
   /// creates an instance with non-nul [eventName]
   ///
   /// [message] is optional
