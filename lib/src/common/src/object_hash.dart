10ede58ecda5ca1f7f63bab67c9927d3febbaedc
Jakub Jankowski <j.jankowski@nomtek.com>
2022-11-08 13:01:22 +0100

    Handle docstrings for constructors/properties that are not documented in canonical api ref in ObjectHash class

--- lib/src/common/src/object_hash.dart
+++ lib/src/common/src/object_hash.dart
@@ -1,8 +1,10 @@
 /// BEGIN LEGACY DOCSTRING
+/// @nodoc
 /// Utility mixin to calculate hashCode values for complex objects
 /// END LEGACY DOCSTRING
 mixin ObjectHash {
   /// BEGIN LEGACY DOCSTRING
+  /// @nodoc
   /// Returns hashCode value for list of fields inside and object
   /// that uses the mixin. Hash value is calculated with a pretty standard hash
   /// algorithm, using 31 as a prime number and adding 0 for null values
