ccf94ab3991a8c7e94383a4ffd456dddf83b609d
Jakub Jankowski <j.jankowski@nomtek.com>
2022-11-09 20:20:55 +0100

    Handle docstrings for constructors/properties that are not documented in canonical api ref for the RemoveMessage class

--- lib/src/push_notifications/src/remote_message.dart
+++ lib/src/push_notifications/src/remote_message.dart
@@ -9,20 +9,41 @@ import 'package:ably_flutter/src/platform/platform_internal.dart';
 /// See https://firebase.google.com/docs/reference/android/com/google/firebase/messaging/RemoteMessage
 /// See https://developer.apple.com/documentation/usernotifications/unnotificationcontent
 /// END LEGACY DOCSTRING
+
+/// BEGIN EDITED DOCSTRING
+/// Represents FCM Message on Android and APNS message on iOS
+/// Both [data] and [notification] are related to corresponding fields in
+/// Android FCM 'RemoteMessage' and iOS 'UNNotificationContent'
+///
+/// See https://firebase.google.com/docs/reference/android/com/google/firebase/messaging/RemoteMessage
+/// See https://developer.apple.com/documentation/usernotifications/unnotificationcontent
+/// END EDITED DOCSTRING
 class RemoteMessage {
   /// BEGIN LEGACY DOCSTRING
   /// Data part of notification, from custom payload
   /// Comes from 'RemoteMessage.data' on Android and
   /// 'UNNotificationContent.userInfo' on iOS
   /// END LEGACY DOCSTRING
+
+  /// BEGIN EDITED DOCSTRING
+  /// Data part of notification, from custom payload.
+  ///
+  /// Comes from 'RemoteMessage.data' on Android and
+  /// 'UNNotificationContent.userInfo' on iOS
+  /// END EDITED DOCSTRING
   Map<String, dynamic> data;
 
   /// BEGIN LEGACY DOCSTRING
   /// Notification part of push message
   /// END LEGACY DOCSTRING
+
+  /// BEGIN EDITED DOCSTRING
+  /// Notification part of push message
+  /// END EDITED DOCSTRING
   Notification? notification;
 
   /// BEGIN LEGACY DOCSTRING
+  /// @nodoc
   /// Initializes an instance with [data] set to empty map
   /// END LEGACY DOCSTRING
   RemoteMessage({
@@ -31,6 +52,7 @@ class RemoteMessage {
   }) : data = data ??= {};
 
   /// BEGIN LEGACY DOCSTRING
+  /// @nodoc
   /// create instance from a map
   /// END LEGACY DOCSTRING
   factory RemoteMessage.fromMap(Map<String, dynamic> map) => RemoteMessage(
