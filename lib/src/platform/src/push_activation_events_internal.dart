0b3f886faac92f0194c8d7af2f914665ec4626a2
Jakub Jankowski <j.jankowski@nomtek.com>
2022-11-09 11:44:45 +0100

    Handle docstrings for constructors/properties that are not documented in canonical api ref in PushActivationEventsInternal class

--- lib/src/platform/src/push_activation_events_internal.dart
+++ lib/src/platform/src/push_activation_events_internal.dart
@@ -4,23 +4,27 @@ import 'dart:async';
 
 import 'package:ably_flutter/ably_flutter.dart';
 
+/// @nodoc
 /// package-private implementation of [PushActivationEvents]
 /// used internally by [Push] instances
 // FIXME: StreamControllers here may leak, we should add a `close()` method
 // See: https://github.com/ably/ably-flutter/issues/382
 class PushActivationEventsInternal extends PushActivationEvents {
   /// BEGIN LEGACY DOCSTRING
+  /// @nodoc
   /// [StreamController] used to handle onActivate events
   /// END LEGACY DOCSTRING
   StreamController<ErrorInfo?> onActivateStreamController = StreamController();
 
   /// BEGIN LEGACY DOCSTRING
+  /// @nodoc
   /// [StreamController] used to handle onDeactivate events
   /// END LEGACY DOCSTRING
   StreamController<ErrorInfo?> onDeactivateStreamController =
       StreamController();
 
   /// BEGIN LEGACY DOCSTRING
+  /// @nodoc
   /// [StreamController] used to handle onUpdateFailed events
   /// END LEGACY DOCSTRING
   StreamController<ErrorInfo> onUpdateFailedStreamController =
