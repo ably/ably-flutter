987b888e23bce4061fe1acb42190077504551bc2
Jakub Jankowski <j.jankowski@nomtek.com>
2022-11-09 20:00:39 +0100

    Handle docstrings for constructors/properties that are not documented in canonical api ref for the Notification class

--- lib/src/push_notifications/src/notification.dart
+++ lib/src/push_notifications/src/notification.dart
@@ -7,21 +7,39 @@ import 'package:meta/meta.dart';
 /// See https://firebase.google.com/docs/reference/android/com/google/firebase/messaging/RemoteMessage.Notification
 /// See https://developer.apple.com/documentation/usernotifications/unnotificationcontent
 /// END LEGACY DOCSTRING
+
+/// BEGIN EDITED DOCSTRING
+/// Notification sent by APNS or FCM
+///
+/// See https://firebase.google.com/docs/reference/android/com/google/firebase/messaging/RemoteMessage.Notification
+/// See https://developer.apple.com/documentation/usernotifications/unnotificationcontent
+/// END EDITED DOCSTRING
 @immutable
 class Notification {
   /// BEGIN LEGACY DOCSTRING
   /// Title of notification, comes from 'UNNotificationContent.title' on iOS
   /// and 'RemoteMessage.notification.title' on Android
   /// END LEGACY DOCSTRING
+
+  /// BEGIN EDITED DOCSTRING
+  /// Title of a notification, comes from 'UNNotificationContent.title' on iOS
+  /// and 'RemoteMessage.notification.title' on Android
+  /// END EDITED DOCSTRING
   final String title;
 
   /// BEGIN LEGACY DOCSTRING
   /// Title of notification, comes from 'UNNotificationContent.body' on iOS
   /// and 'RemoteMessage.notification.body' on Android
   /// END LEGACY DOCSTRING
+
+  /// BEGIN LEGACY DOCSTRING
+  /// Body of a notification, comes from 'UNNotificationContent.body' on iOS
+  /// and 'RemoteMessage.notification.body' on Android
+  /// END LEGACY DOCSTRING
   final String? body;
 
   /// BEGIN LEGACY DOCSTRING
+  /// @nodoc
   /// Initializes an instance without any defaults
   /// END LEGACY DOCSTRING
   const Notification({
@@ -30,6 +48,7 @@ class Notification {
   });
 
   /// BEGIN LEGACY DOCSTRING
+  /// @nodoc
   /// Creates an instance from the map
   /// END LEGACY DOCSTRING
   factory Notification.fromMap(Map<String, dynamic> map) => Notification(
