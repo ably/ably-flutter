46ae1b6413a821e0588e3d189342ddd345628a48
Jakub Jankowski <j.jankowski@nomtek.com>
2022-11-09 20:00:53 +0100

    Handle docstrings for constructors/properties that are not documented in canonical api ref for the Push class

--- lib/src/push_notifications/src/push.dart
+++ lib/src/push_notifications/src/push.dart
@@ -18,25 +18,38 @@ class Push extends PlatformObject {
   /// An instance to access activation events related to push, such as device
   /// activation, deactivation and notification permissions.
   /// END LEGACY DOCSTRING
+
+  /// BEGIN EDITED DOCSTRING
+  /// A static object that can be used to access activation events related to
+  /// push, such as device activation, deactivation and notification
+  /// permissions.
+  /// END EDITED DOCSTRING
   static PushActivationEvents activationEvents = PushActivationEventsInternal();
 
   /// BEGIN LEGACY DOCSTRING
   /// An instance to access message events related to push
   /// END LEGACY DOCSTRING
+
+  /// BEGIN EDITED DOCSTRING
+  /// A static object that can be used to access message events related to push
+  /// END EDITED DOCSTRING
   static PushNotificationEvents notificationEvents =
       PushNotificationEventsInternal();
 
   /// BEGIN LEGACY DOCSTRING
+  /// @nodoc
   /// A rest client used platform side to invoke push notification methods
   /// END LEGACY DOCSTRING
   final Rest? rest;
 
   /// BEGIN LEGACY DOCSTRING
+  /// @nodoc
   /// A realtime client used platform side to invoke push notification methods
   /// END LEGACY DOCSTRING
   final Realtime? realtime;
 
   /// BEGIN LEGACY DOCSTRING
+  /// @nodoc
   /// Pass an Ably realtime or rest client.
   /// END LEGACY DOCSTRING
   Push({
@@ -102,6 +115,36 @@ class Push extends PlatformObject {
   ///
   /// [Apple docs](https://developer.apple.com/documentation/usernotifications/unusernotificationcenter/1649527-requestauthorization)
   /// END LEGACY DOCSTRING
+
+  /// BEGIN EDITED DOCSTRING
+  /// Request permission from the user to show them notifications. This is
+  /// required to show user notifications. Otherwise, notifications may
+  /// silently get received by the application.
+  ///
+  /// This always returns true on Android, since you don't need permissions
+  /// to show notifications to the user.
+  ///
+  /// You can customize the request by specifying whether the app should
+  /// display [alert]s, play [sound]s, update the app's [badge], display
+  /// notifications in the [carPlay] environment, whether the app should ignore
+  /// the mute switch and Do Not Disturb by setting [criticalAlert], and set
+  /// whether the system should display a button for in-app notification
+  /// settings (iOS 12+) with [providesAppNotificationSettings].
+  /// With [provisional] you can configure whether the app should send
+  /// notifications on a trial basis, by delaying the permission request until
+  /// the user first sees the first notification. iOS 12+. In that case a
+  /// notification is first delivered quietly, and the user gets an option
+  /// to deliver it more prominently. If [provisional] is true, the permission
+  /// request alert will not be shown to the user, regardless of other options
+  /// passed in. For more information, see
+  /// [Use Provisional Authorization to Send Trial Notifications](https://developer.apple.com/documentation/usernotifications/asking_permission_to_use_notifications?language=objc)
+  /// You can also choose if Siri should have an ability to automatically read
+  /// out messages over AirPods (matters on iOS 13+. Deprecated in iOS 15+,
+  /// because it is automatically/always granted).
+  ///
+  /// Asynchrously returns a [bool] indicating whether the permission was
+  /// granted
+  /// END EDITED DOCSTRING
   Future<bool> requestPermission({
     bool alert = true,
     bool announcement = true,
@@ -135,6 +178,13 @@ class Push extends PlatformObject {
   ///
   /// [Apple docs](https://developer.apple.com/documentation/usernotifications/unusernotificationcenter/1649524-getnotificationsettings)
   /// END LEGACY DOCSTRING
+
+  /// BEGIN EDITED DOCSTRING
+  /// Gets the iOS notification settings ([UNNotificationSettings]) for
+  /// the application.
+  ///
+  /// More details in the [Apple docs](https://developer.apple.com/documentation/usernotifications/unusernotificationcenter/1649524-getnotificationsettings)
+  /// END EDITED DOCSTRING
   Future<UNNotificationSettings> getNotificationSettings() async {
     if (io.Platform.isIOS) {
       return invokeRequest<UNNotificationSettings>(
@@ -165,6 +215,7 @@ class Push extends PlatformObject {
   Future<void> deactivate() => invoke(PlatformMethod.pushDeactivate);
 
   /// BEGIN LEGACY DOCSTRING
+  /// @nodoc
   /// Resets activation state of Android push device by removing
   /// device data from Android SharedPreferences. After this operation, device
   /// is recognized as a completely new push device and all device data
