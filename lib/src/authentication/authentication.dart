df8c24057250121fd0160710cd3133a4d8520380
Jakub Jankowski <j.jankowski@nomtek.com>
2022-11-10 20:45:46 +0100

    Remove export statements for files that were removed from the project.

--- lib/src/authentication/authentication.dart
+++ lib/src/authentication/authentication.dart
@@ -1,7 +1,6 @@
 export 'src/auth.dart';
 export 'src/auth_options.dart';
 export 'src/client_options.dart';
-export 'src/http_auth_type.dart';
 export 'src/token_details.dart';
 export 'src/token_params.dart';
 export 'src/token_request.dart';
--- lib/src/common/common.dart
+++ lib/src/common/common.dart
@@ -1,4 +1,2 @@
 export 'src/channels.dart';
-export 'src/event_emitter.dart';
-export 'src/event_listener.dart';
 export 'src/http_paginated_response.dart';
