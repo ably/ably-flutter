529080191fcec308973a8a953f0af1f4b77eabed
ikbalkaya <ceikbal@gmail.com>
2023-04-24 10:29:42 +0100

    Conflict resolutions from merge

--- lib/src/realtime/src/realtime_auth.dart
+++ lib/src/realtime/src/realtime_auth.dart
@@ -3,7 +3,6 @@
 import 'package:ably_flutter/ably_flutter.dart';
 import 'package:ably_flutter/src/platform/platform_internal.dart';
 
-<<<<<<< HEAD
 /// See [Auth] for more information.
 class RealtimeAuth extends Auth {
   final Realtime _realtime;
@@ -40,38 +39,3 @@ class RealtimeAuth extends Auth {
         TxTransportKeys.params: tokenParams
       });
 }
-=======
-class RealtimeAuth extends Auth{
-
-  final Realtime realtime;
-  RealtimeAuth(this.realtime);
-
-  @override
-  Future<TokenDetails?> authorize({AuthOptions? authOptions, TokenParams?
-  tokenParams}) => realtime.invoke<TokenDetails>(PlatformMethod
-        .realtimeAuthAuthorize, {
-      TxTransportKeys.options: authOptions,
-      TxTransportKeys.params: tokenParams
-    });
-
-  @override
-  Future<String?> get clientId async => await realtime.invoke<String>(PlatformMethod
-      .realtimeAuthGetClientId);
-
-  @override
-  Future<TokenRequest?> createTokenRequest({AuthOptions? authOptions,
-    TokenParams? tokenParams}) => realtime.invoke<TokenRequest>(PlatformMethod
-        .realtimeAuthCreateTokenRequest, {
-      TxTransportKeys.options: authOptions,
-      TxTransportKeys.params: tokenParams
-    });
-
-  @override
-  Future<TokenDetails?> requestToken({AuthOptions? authOptions, TokenParams?
-  tokenParams}) => realtime.invoke<TokenDetails>(PlatformMethod
-        .realtimeAuthRequestToken, {
-      TxTransportKeys.options: authOptions,
-      TxTransportKeys.params: tokenParams
-    });
-}
->>>>>>> parent of 179e43f0 (Code style and formatting changes)
--- lib/src/rest/src/rest_auth.dart
+++ lib/src/rest/src/rest_auth.dart
@@ -3,7 +3,6 @@
 import 'package:ably_flutter/ably_flutter.dart';
 import 'package:ably_flutter/src/platform/platform_internal.dart';
 
-<<<<<<< HEAD
 /// See [Auth] for more information.
 class RestAuth extends Auth {
   final Rest _rest;
@@ -39,38 +38,3 @@ class RestAuth extends Auth {
         TxTransportKeys.params: tokenParams
       });
 }
-=======
-class RestAuth extends Auth{
-
-  final Rest rest;
-  RestAuth(this.rest);
-
-  @override
-  Future<TokenDetails?> authorize({AuthOptions? authOptions, TokenParams?
-  tokenParams}) => rest.invoke<TokenDetails>(PlatformMethod
-        .restAuthAuthorize, {
-      TxTransportKeys.options: authOptions,
-      TxTransportKeys.params: tokenParams
-    });
-
-  @override
-  Future<String?> get clientId async => await rest.invoke<String>(PlatformMethod
-      .restAuthGetClientId);
-
-  @override
-  Future<TokenRequest?> createTokenRequest({AuthOptions? authOptions,
-    TokenParams? tokenParams}) => rest.invoke<TokenRequest>(PlatformMethod
-        .restAuthCreateTokenRequest, {
-      TxTransportKeys.options: authOptions,
-      TxTransportKeys.params: tokenParams
-    });
-
-  @override
-  Future<TokenDetails?> requestToken({AuthOptions? authOptions, TokenParams?
-  tokenParams}) => rest.invoke<TokenDetails>(PlatformMethod
-        .restAuthRequestToken, {
-      TxTransportKeys.options: authOptions,
-      TxTransportKeys.params: tokenParams
-    });
-}
->>>>>>> parent of 179e43f0 (Code style and formatting changes)
