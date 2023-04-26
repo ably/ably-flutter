6ac176204355356fe63d82b857e253255117919b
ikbalkaya <ceikbal@gmail.com>
2023-04-26 10:48:14 +0100

    Improve create token request test

--- test_integration/lib/test/rest/rest_create_token_request_test.dart
+++ test_integration/lib/test/rest/rest_create_token_request_test.dart
@@ -18,9 +18,25 @@ Future<Map<String, dynamic>> testRestCreateTokenRequest({
   final rest = Rest(
     options: clientOptionsForToken,
   );
-  final request = await rest.auth?.createTokenRequest();
-  if (request == null) {
+  const tokenParams = TokenParams(ttl: 20000, capability: '{"testchannel":["publish"]}',
+      clientId: 'testclient');
+
+  final authOptions = AuthOptions(
+      key: appKey,
+      useTokenAuth: true,
+      authMethod: 'GET',
+      authHeaders: {'heardkey': 'headervalue'},
+      authUrl: "bogus",
+      authParams: {'paramkey': 'paramvalue'});
+  final request = await rest.auth?.createTokenRequest(authOptions: authOptions,
+      tokenParams: tokenParams);
+  if (request?.ttl != tokenParams.ttl ||
+      request?.clientId != tokenParams.clientId ||
+      request?.capability != tokenParams.capability) {
     throw Error();
   }
-  return {'handle': await rest.handle};
+
+  return {
+    'handle': await rest.handle,
+  };
 }
