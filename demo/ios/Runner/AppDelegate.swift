369741c1d01edb672a3845fd24463b384bb4ec45
Marat Al <m.alekperov@gmail.com>
2025-02-11 17:47:21 +0100

    In attempt to reproduce issue https://github.com/ably/ably-flutter/issues/557 I've created this flutter demo project from scratch. Still wasn't able to reproduce. Leaving it here for reference.

---
+++ demo/ios/Runner/AppDelegate.swift
@@ -0,0 +1,18 @@
+import Flutter
+import UIKit
+
+@main
+@objc class AppDelegate: FlutterAppDelegate {
+    
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+
+    override func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        NSLog("application:didFailToRegisterForRemoteNotificationsWithError was called with error: %@", error.localizedDescription)
+    }
+}
